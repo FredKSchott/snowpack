0d649a58bb0d197009014ced2f0d18adca5435ee
Fred K. Schott <fkschott@gmail.com>
2019-03-09 13:30:44 -0800

    configure prettier properly

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ import yargs from 'yargs-parser';
 import * as rollup from 'rollup';
 import rollupPluginNodeResolve from 'rollup-plugin-node-resolve';
 import rollupPluginCommonjs from 'rollup-plugin-commonjs';
-import { terser as rollupPluginTerser } from "rollup-plugin-terser";
+import {terser as rollupPluginTerser} from 'rollup-plugin-terser';
 import rollupPluginReplace from 'rollup-plugin-replace';
 
 export interface InstallOptions {
@@ -39,17 +39,22 @@ function logError(msg) {
   spinner.fail();
 }
 
-function transformWebModuleFilename(depName:string):string {
+function transformWebModuleFilename(depName: string): string {
   return depName.replace('/', '--');
 }
 
-export async function install(arrayOfDeps: string[], {isCleanInstall, destLoc, isWhitelist, isStrict, isOptimized}: InstallOptions) {
+export async function install(
+  arrayOfDeps: string[],
+  {isCleanInstall, destLoc, isWhitelist, isStrict, isOptimized}: InstallOptions,
+) {
   if (arrayOfDeps.length === 0) {
     logError('no dependencies found.');
     return;
   }
   if (!fs.existsSync(path.join(cwd, 'node_modules'))) {
-    logError('no node_modules/ directory exists. Run "npm install" in your project before running @pika/web.');
+    logError(
+      'no node_modules/ directory exists. Run "npm install" in your project before running @pika/web.',
+    );
     return;
   }
 
@@ -61,7 +66,9 @@ export async function install(arrayOfDeps: string[], {isCleanInstall, destLoc, i
   for (const dep of arrayOfDeps) {
     const depLoc = path.join(cwd, 'node_modules', dep);
     if (!fs.existsSync(depLoc)) {
-        logError(`dependency "${dep}" not found in your node_modules/ directory. Did you run npm install?`);
+      logError(
+        `dependency "${dep}" not found in your node_modules/ directory. Did you run npm install?`,
+      );
       return;
     }
     const depManifestLoc = path.join(cwd, 'node_modules', dep, 'package.json');
@@ -69,7 +76,15 @@ export async function install(arrayOfDeps: string[], {isCleanInstall, destLoc, i
     if (!depManifest.module) {
       if (isWhitelist) {
         logError(`dependency "${dep}" has no ES "module" entrypoint.`);
-        console.log('\n' + chalk.italic(`Tip: Find modern, web-ready packages at ${chalk.underline('https://pikapkg.com/packages')}`) + '\n');
+        console.log(
+          '\n' +
+            chalk.italic(
+              `Tip: Find modern, web-ready packages at ${chalk.underline(
+                'https://pikapkg.com/packages',
+              )}`,
+            ) +
+            '\n',
+        );
         return false;
       }
       continue;
@@ -80,31 +95,33 @@ export async function install(arrayOfDeps: string[], {isCleanInstall, destLoc, i
   const inputOptions = {
     input: depObject,
     plugins: [
-      !isStrict && rollupPluginReplace({
-        'process.env.NODE_ENV': isOptimized ? '"production"' : '"development"'
-      }),
+      !isStrict &&
+        rollupPluginReplace({
+          'process.env.NODE_ENV': isOptimized ? '"production"' : '"development"',
+        }),
       rollupPluginNodeResolve({
         module: true, // Default: true
-        jsnext: false,  // Default: false
-        main: !isStrict,  // Default: true
-        browser: false,  // Default: false
+        jsnext: false, // Default: false
+        main: !isStrict, // Default: true
+        browser: false, // Default: false
         modulesOnly: isStrict, // Default: false
-        extensions: [ '.mjs', '.js', '.json' ],  // Default: [ '.mjs', '.js', '.json', '.node' ]
+        extensions: ['.mjs', '.js', '.json'], // Default: [ '.mjs', '.js', '.json', '.node' ]
         // whether to prefer built-in modules (e.g. `fs`, `path`) or local ones with the same names
-        preferBuiltins: false,  // Default: true
+        preferBuiltins: false, // Default: true
       }),
-      !isStrict && rollupPluginCommonjs({
-        extensions: [ '.js', '.cjs' ],  // Default: [ '.js' ]
-      }),
-      isOptimized && rollupPluginTerser()
-    ]
+      !isStrict &&
+        rollupPluginCommonjs({
+          extensions: ['.js', '.cjs'], // Default: [ '.js' ]
+        }),
+      isOptimized && rollupPluginTerser(),
+    ],
   };
   const outputOptions = {
     dir: destLoc,
-    format: "esm" as 'esm',
+    format: 'esm' as 'esm',
     sourcemap: true,
     exports: 'named' as 'named',
-    chunkFileNames: "common/[name]-[hash].js"
+    chunkFileNames: 'common/[name]-[hash].js',
   };
   const packageBundle = await rollup.rollup(inputOptions);
   await packageBundle.write(outputOptions);
@@ -112,21 +129,34 @@ export async function install(arrayOfDeps: string[], {isCleanInstall, destLoc, i
 }
 
 export async function cli(args: string[]) {
-  const {help, optimize = false, strict = false, clean = false, dest = "web_modules"} = yargs(args);
+  const {help, optimize = false, strict = false, clean = false, dest = 'web_modules'} = yargs(args);
   const destLoc = path.join(cwd, dest);
 
-	if (help) {
+  if (help) {
     showHelp();
     process.exit(0);
   }
 
   const cwdManifest = require(path.join(cwd, 'package.json'));
-  const isWhitelist = !!cwdManifest && !!cwdManifest['@pika/web'] && !!cwdManifest['@pika/web'].webDependencies;
-  const arrayOfDeps = isWhitelist ? cwdManifest['@pika/web'].webDependencies : Object.keys(cwdManifest.dependencies || {});
+  const isWhitelist =
+    !!cwdManifest && !!cwdManifest['@pika/web'] && !!cwdManifest['@pika/web'].webDependencies;
+  const arrayOfDeps = isWhitelist
+    ? cwdManifest['@pika/web'].webDependencies
+    : Object.keys(cwdManifest.dependencies || {});
   spinner.start();
   const startTime = Date.now();
-  const result = await install(arrayOfDeps, {isCleanInstall: clean, destLoc, isWhitelist, isStrict: strict, isOptimized: optimize});
+  const result = await install(arrayOfDeps, {
+    isCleanInstall: clean,
+    destLoc,
+    isWhitelist,
+    isStrict: strict,
+    isOptimized: optimize,
+  });
   if (result) {
-    spinner.succeed(chalk.green.bold(`@pika/web`) + ` installed web-native dependencies. ` + chalk.dim(`[${((Date.now() - startTime) / 1000).toFixed(2)}s]`));
+    spinner.succeed(
+      chalk.green.bold(`@pika/web`) +
+        ` installed web-native dependencies. ` +
+        chalk.dim(`[${((Date.now() - startTime) / 1000).toFixed(2)}s]`),
+    );
   }
 }
