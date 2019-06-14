c1d97e7c03aa1578d1fb89725ec22c72af24c295
Fred K. Schott <fkschott@gmail.com>
2019-06-13 22:15:18 -0700

    add browser as a main field

--- src/index.ts
+++ src/index.ts
@@ -183,7 +183,7 @@ export async function install(
           'process.env.NODE_ENV': isOptimized ? '"production"' : '"development"',
         }),
        rollupPluginNodeResolve({
-        mainFields: ['module', !isStrict && 'main'].filter(Boolean),
+        mainFields: ['browser', 'module', !isStrict && 'main'].filter(Boolean),
         modulesOnly: isStrict, // Default: false
         extensions: ['.mjs', '.cjs', '.js', '.json'], // Default: [ '.mjs', '.js', '.json', '.node' ]
         // whether to prefer built-in modules (e.g. `fs`, `path`) or local ones with the same names
