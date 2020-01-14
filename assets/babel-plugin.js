81e1c97109b748d69a53d41ad76db881db709e3f
Yuku Kotani <yukukotani@gmail.com>
2020-01-14 11:45:08 +0900

    Implement addVersion opt for babel plugin

--- assets/babel-plugin.js
+++ assets/babel-plugin.js
@@ -1,15 +1,25 @@
 const path = require('path');
+const fs = require('fs');
 // A lame copy-paste from src/index.ts
 function getWebDependencyName(dep) {
   return dep.replace(/\.js$/, '');
 }
 
-function rewriteImport(imp, dir, shouldAddMissingExtension) {
+function getPackageVersion(package) {
+  const packageJsonPath = path.posix.join('./', 'node_modules', package, 'package.json');
+  const json = JSON.parse(fs.readFileSync(packageJsonPath));
+  return json.version;
+}
+
+function rewriteImport(imp, dir, shouldAddMissingExtension, shouldAddVersion) {
   const isSourceImport = imp.startsWith('/') || imp.startsWith('.') || imp.startsWith('\\');
   const isRemoteimport = imp.startsWith('http://') || imp.startsWith('https://');
   dir = dir || 'web_modules';
   if (!isSourceImport && !isRemoteimport) {
-    return path.posix.join('/', dir, `${getWebDependencyName(imp)}.js`);
+    return shouldAddVersion
+      ? path.posix.join('/', dir, `${getWebDependencyName(imp)}.js`) +
+          `?version=${getPackageVersion(imp)}`
+      : path.posix.join('/', dir, `${getWebDependencyName(imp)}.js`);
   }
   if (!isRemoteimport && shouldAddMissingExtension && !path.extname(imp)) {
     return imp + '.js';
@@ -19,13 +29,17 @@ function rewriteImport(imp, dir, shouldAddMissingExtension) {
 
 /**
  * BABEL OPTIONS:
+ *   addVersion         - Adds package version as a query parameter to blow browser cache.
  *   dir                - The web_modules installed location once hosted on the web.
  *                        Defaults to "web_modules", which translates package imports to "/web_modules/PACKAGE_NAME".
  *   optionalExtensions - Adds any missing JS extensions to local/relative imports. Support for these
  *                        partial imports is missing in the browser and being phased out of Node.js, but
  *                        this can be a useful option for migrating an old project to Snowpack.
  */
-module.exports = function pikaWebBabelTransform({types: t}, {optionalExtensions, dir} = {}) {
+module.exports = function pikaWebBabelTransform(
+  {types: t},
+  {optionalExtensions, dir, addVersion} = {},
+) {
   return {
     visitor: {
       CallExpression(path, {file, opts}) {
@@ -40,7 +54,7 @@ module.exports = function pikaWebBabelTransform({types: t}, {optionalExtensions,
         }
 
         source.replaceWith(
-          t.stringLiteral(rewriteImport(source.node.value, dir, optionalExtensions)),
+          t.stringLiteral(rewriteImport(source.node.value, dir, optionalExtensions, addVersion)),
         );
       },
       'ImportDeclaration|ExportNamedDeclaration|ExportAllDeclaration'(path, {file, opts}) {
@@ -52,7 +66,7 @@ module.exports = function pikaWebBabelTransform({types: t}, {optionalExtensions,
         }
 
         source.replaceWith(
-          t.stringLiteral(rewriteImport(source.node.value, dir, optionalExtensions)),
+          t.stringLiteral(rewriteImport(source.node.value, dir, optionalExtensions, addVersion)),
         );
       },
     },
