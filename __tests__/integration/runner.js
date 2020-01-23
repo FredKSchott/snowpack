08dd1be5750c12e19e9463a9f2592803c6eee8e5
Fred K. Schott <fkschott@gmail.com>
2020-01-23 13:00:20 -0800

    get more output for testing

--- __tests__/integration/runner.js
+++ __tests__/integration/runner.js
@@ -30,6 +30,8 @@ for (const testName of readdirSync(__dirname)) {
     // Test Output
     const expectedOutputLoc = path.join(__dirname, testName, 'expected-output.txt');
     const expectedOutput = await fs.readFile(expectedOutputLoc, {encoding: 'utf8'});
+    console.log(stripWhitespace(stripBenchmark(all)));
+    console.log(JSON.stringify(expectedOutput));
     assert.strictEqual(stripWhitespace(stripBenchmark(all)), expectedOutput);
 
     const expectedWebDependenciesLoc = path.join(__dirname, testName, 'expected-install');
