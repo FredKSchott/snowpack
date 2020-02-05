40a9f71d34269fa1f8a6fd89cbd715a84bda0b9c
alex-saunders <alexjrsaunders@gmail.com>
2020-02-05 13:02:52 +0000

    flesh out rollup plugin

--- src/rollup-plugin-dependency-info.ts
+++ src/rollup-plugin-dependency-info.ts
@@ -2,21 +2,67 @@ import fs from 'fs';
 import path from 'path';
 import {OutputOptions, OutputBundle} from 'rollup';
 
-export function rollupPluginDependencyInfo() {
+type Dependency = {
+  fileName: string;
+  size: number;
+  delta?: number;
+};
+type Dependencies = {
+  [filePath: string]: Dependency;
+};
+type DependencyType = 'direct' | 'shared';
+export type DependencyInfoOutput = Record<DependencyType, Dependencies>;
+
+export function rollupPluginDependencyInfo(cb: (dependencyInfo: DependencyInfoOutput) => void) {
+  let outputDir: string;
+  let cache: {
+    [fileName: string]: number;
+  } = {};
+  let output: DependencyInfoOutput = {
+    direct: {},
+    shared: {},
+  };
+
+  const compareDependencies = (files: string[], type: DependencyType) => {
+    files.forEach(file => {
+      const filePath = path.join(outputDir, file);
+      const {size} = fs.statSync(filePath);
+      output[type][file] = {
+        fileName: path.basename(file),
+        size,
+      };
+
+      if (cache[file]) {
+        const delta = (size - cache[file]) / 1000;
+        output[type][file].delta = delta;
+      }
+    });
+  };
+
   return {
     generateBundle(options: OutputOptions, bundle: OutputBundle) {
-      // console.log(options);
-      const files = fs.readdirSync(options.dir);
-      // console.log(files);
+      outputDir = options.dir;
 
       for (let fileName of Object.keys(bundle)) {
-        const previousModuleFile = fs.existsSync(path.join(options.dir, fileName));
-        if (previousModuleFile) {
-          console.log('file already exists!', fileName);
+        const filePath = path.join(outputDir, fileName);
+        if (fs.existsSync(filePath)) {
+          const {size} = fs.statSync(filePath);
+          cache[fileName] = size;
         }
-
-        // console.log(fileName, bundle[fileName].type);
       }
+    },
+    writeBundle(bundle: OutputBundle) {
+      const files = Object.keys(bundle);
+
+      const [directDependencies, sharedDependencies] = files.reduce(
+        ([d, s], f) => (f.startsWith('common') ? [d, [...s, f]] : [[...d, f], s]),
+        [[], []],
+      );
+
+      compareDependencies(directDependencies, 'direct');
+      compareDependencies(sharedDependencies, 'shared');
+
+      cb(output);
     },
   };
 }
