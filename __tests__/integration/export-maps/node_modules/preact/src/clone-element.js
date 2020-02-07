cb9a95676c1d2ee69d0f37f545ff0ff9af67e65b
Fred K. Schott <fkschott@gmail.com>
2020-02-07 10:00:50 -0800

    add export map support for node v13

---
+++ __tests__/integration/export-maps/node_modules/preact/src/clone-element.js
@@ -0,0 +1,20 @@
+import { assign } from './util';
+import { EMPTY_ARR } from './constants';
+import { createVNode } from './create-element';
+
+/**
+ * Clones the given VNode, optionally adding attributes/props and replacing its children.
+ * @param {import('./internal').VNode} vnode The virtual DOM element to clone
+ * @param {object} props Attributes/props to add when cloning
+ * @param {Array<import('./index').ComponentChildren>} rest Any additional arguments will be used as replacement children.
+ */
+export function cloneElement(vnode, props) {
+	props = assign(assign({}, vnode.props), props);
+	if (arguments.length > 2) props.children = EMPTY_ARR.slice.call(arguments, 2);
+	return createVNode(
+		vnode.type,
+		props,
+		props.key || vnode.key,
+		props.ref || vnode.ref
+	);
+}
---
+++ __tests__/integration/export-maps/node_modules/preact/src/util.js
@@ -0,0 +1,22 @@
+/**
+ * Assign properties from `props` to `obj`
+ * @template O, P The obj and props types
+ * @param {O} obj The object to copy properties to
+ * @param {P} props The object to copy properties from
+ * @returns {O & P}
+ */
+export function assign(obj, props) {
+	for (let i in props) obj[i] = props[i];
+	return /** @type {O & P} */ (obj);
+}
+
+/**
+ * Remove a child node from its parent if attached. This is a workaround for
+ * IE11 which doesn't support `Element.prototype.remove()`. Using this function
+ * is smaller than including a dedicated polyfill.
+ * @param {Node} node The node to remove
+ */
+export function removeNode(node) {
+	let parentNode = node.parentNode;
+	if (parentNode) parentNode.removeChild(node);
+}
