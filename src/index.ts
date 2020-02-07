b9ec43a992aea3d117bf562d9404b59de7f617eb
Fred K. Schott <fkschott@gmail.com>
2020-02-07 10:21:20 -0800

    add no warnings flag for test output matching

--- src/index.ts
+++ src/index.ts
@@ -116,11 +116,11 @@ function detectExports(filePath: string): string[] | undefined {
 }
 
 /**
- * Given a package name, look for that package's package.json manifest. 
- * Return both the manifest location (if believed to exist) and the 
+ * Given a package name, look for that package's package.json manifest.
+ * Return both the manifest location (if believed to exist) and the
  * manifest itself (if found).
- *  
- * NOTE: You used to be able to require() a package.json file directly, 
+ *
+ * NOTE: You used to be able to require() a package.json file directly,
  * but now with export map support in Node v13 that's no longer possible.
  */
 function resolveDependencyManifest(dep: string, cwd: string) {
@@ -132,10 +132,11 @@ function resolveDependencyManifest(dep: string, cwd: string) {
     const searchPath = `${path.sep}node_modules${path.sep}${dep.replace('/', path.sep)}`;
     const indexOfSearch = fullPath.lastIndexOf(searchPath);
     if (indexOfSearch >= 0) {
-      const manifestPath = fullPath.substring(0, indexOfSearch + searchPath.length + 1) + 'package.json';
+      const manifestPath =
+        fullPath.substring(0, indexOfSearch + searchPath.length + 1) + 'package.json';
       result[0] = manifestPath;
       const manifestStr = fs.readFileSync(manifestPath, {encoding: 'utf8'});
-      result[1] =  JSON.parse(manifestStr);
+      result[1] = JSON.parse(manifestStr);
     }
   } catch (err) {
     // ignore
@@ -515,9 +516,12 @@ export async function cli(args: string[]) {
     );
   }
 
-  //If an error happened, set the exit code so that programmatic usage of the CLI knows.
+  // If an error happened, set the exit code so that programmatic usage of the CLI knows.
+  // We were seeing race conditions here, so add a little buffer.
   if (spinnerHasError) {
-    spinner.warn(chalk(`Finished with warnings.`));
-    process.exitCode = 1;
+    setTimeout(() => {
+      spinner.warn(chalk(`Finished with warnings.`));
+      process.exitCode = 1;
+    }, 5);
   }
 }
