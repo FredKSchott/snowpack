44c74ccadbc14678fe0f33186de0cc4b0810c6b1
Fred K. Schott <fkschott@gmail.com>
2020-05-05 23:16:02 -0700

    default to node_env prod at install time

--- src/index.ts
+++ src/index.ts
@@ -220,7 +220,7 @@ function getRollupReplaceKeys(env: EnvVarReplacements): Record<string, string> {
       return acc;
     },
     {
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development'),
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'production'),
       'process.env.': '({}).',
     },
   );
