e6155cfce9a150556e5d18d0cd6bc3def4fe5a78
Fred K. Schott <fkschott@gmail.com>
2020-05-05 23:12:15 -0700

    update svelte plugin to respect node_env

--- packages/create-snowpack-app/packages/plugin-svelte/plugin.js
+++ packages/create-snowpack-app/packages/plugin-svelte/plugin.js
@@ -3,6 +3,8 @@ const fs = require("fs");
 
 exports.build = function build(fileLoc) {
   const fileSource = fs.readFileSync(fileLoc, { encoding: "utf-8" });
-  const result = svelte.compile(fileSource);
+  const result = svelte.compile(fileSource, {
+    dev: process.env.NODE_ENV !== "production",
+  });
   return { result: result.js.code };
 };
