9e9494191f2bfe020194aae7681a0a67301b49d4
Fred K. Schott <fkschott@gmail.com>
2020-05-05 23:08:52 -0700

    update vue 3 template

--- packages/create-snowpack-app/templates/app-template-vue/jest.config.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  ...require("@snowpack/app-scripts-vue/jest.config.js")(),
-};
--- packages/create-snowpack-app/templates/app-template-vue/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render } from "@testing-library/vue";
-import App from "./App";
-
-test("renders learn svelte link", () => {
-  const { getByText } = render(App);
-  const linkElement = getByText(/learn vue/i);
-  expect(linkElement).toBeInTheDocument();
-});
