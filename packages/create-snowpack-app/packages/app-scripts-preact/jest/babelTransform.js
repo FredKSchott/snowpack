2830f07f3932679dc3e660128e14b4ae235ab858
Fred K. Schott <fkschott@gmail.com>
2020-05-08 14:24:50 -0700

    format

--- packages/create-snowpack-app/packages/app-scripts-preact/jest/babelTransform.js
+++ packages/create-snowpack-app/packages/app-scripts-preact/jest/babelTransform.js
@@ -11,15 +11,21 @@ const babelJest = require("babel-jest");
 
 module.exports = babelJest.createTransformer({
   presets: [
-    [require.resolve("@babel/preset-env"), {
-      targets: {
-        node: 'current',
+    [
+      require.resolve("@babel/preset-env"),
+      {
+        targets: {
+          node: "current",
+        },
       },
-    }],
-    [require.resolve("@babel/preset-react"), {
-      pragma: "h",
-      pragmaFrag: "Fragment"
-    }],
+    ],
+    [
+      require.resolve("@babel/preset-react"),
+      {
+        pragma: "h",
+        pragmaFrag: "Fragment",
+      },
+    ],
     require.resolve("@babel/preset-typescript"),
   ],
   plugins: [
