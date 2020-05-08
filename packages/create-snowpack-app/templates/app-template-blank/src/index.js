9969c03aacf836e54378b2d97292ac61a5759723
Fred K. Schott <fkschott@gmail.com>
2020-05-07 18:47:17 -0700

    update templates missing files

--- packages/create-snowpack-app/templates/app-template-blank/src/index.js
+++ packages/create-snowpack-app/templates/app-template-blank/src/index.js
@@ -5,12 +5,7 @@
 
 import confetti from "canvas-confetti";
 
-const canvas = document.createElement("canvas");
-canvas.width = "960";
-canvas.height = "960";
-document.body.appendChild(canvas);
-
-confetti.create(canvas, {
+confetti.create(document.getElementById("canvas"), {
   resize: true,
   useWorker: true,
 })({ particleCount: 200, spread: 200 });
