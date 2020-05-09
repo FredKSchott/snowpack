39bcfea833ac59c8237a61effbe2023a42e5fe2f
Fred K. Schott <fkschott@gmail.com>
2020-05-09 16:44:30 -0700

    add hmr stub package

---
+++ packages/create-snowpack-app/packages/hmr/index.js
@@ -0,0 +1,7 @@
+/**
+ * This is the production @snowpack/hmr code.
+ *
+ * `snowpack dev` - Replaces this with the hydrated hmr logic.
+ * `snowpack build` - Uses this empty stub as a no-op.
+ */
+export function apply() {}
