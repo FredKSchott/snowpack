47c367a2c54469a71e32f92c89c042a811dd79dd
Fred K. Schott <fkschott@gmail.com>
2020-05-17 03:49:01 -0700

    add preprocess support to svelte plugin (#50)
    Co-authored-by: jamesopstad

--- packages/create-snowpack-app/packages/plugin-svelte/plugin.js
+++ packages/create-snowpack-app/packages/plugin-svelte/plugin.js
@@ -1,10 +1,29 @@
 const svelte = require("svelte/compiler");
 const fs = require("fs");
+const path = require("path");
 
-exports.build = function build(fileLoc) {
+let userSvelteConfig = {};
+try {
+  userSvelteConfig = require(path.join(process.cwd(), "svelte.config.js"));
+} catch (err) {
+  // no user-provided config found, safe to ignore
+}
+
+exports.build = async function build(fileLoc) {
+  const { preprocess: preprocessOptions, ...svelteOptions } = userSvelteConfig;
   const fileSource = fs.readFileSync(fileLoc, { encoding: "utf-8" });
-  const result = svelte.compile(fileSource, {
+  let codeToCompile = fileSource;
+  // PRE-PROCESS
+  if (preprocessOptions) {
+    ({ code: codeToCompile } = await svelte.preprocess(
+      fileSource,
+      preprocessOptions
+    ));
+  }
+  // COMPILE
+  const result = svelte.compile(codeToCompile, {
     dev: process.env.NODE_ENV !== "production",
+    ...svelteOptions,
   });
   return { result: result.js.code };
 };
