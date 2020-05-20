176cd29eaf097565c836709a26862e739680d0c9
Marc Lajoie <57726977+lajoiemedia@users.noreply.github.com>
2020-05-20 10:55:26 -0400

    added @snowpack/plugin-bundle-webpack (#57)
    * added @snowpack/plugin-bundle-webpack
    
    * File loader test pattern is now .*
    
    * Excluded other patterns in file-loader, otherwise breaks
    
    * Fix asset pattern

---
+++ packages/create-snowpack-app/packages/plugin-bundle-webpack/plugin.js
@@ -0,0 +1,140 @@
+const fs = require("fs");
+const path = require("path");
+const webpack = require("webpack");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const jsdom = require("jsdom");
+const CopyPlugin = require("copy-webpack-plugin");
+const { JSDOM } = jsdom;
+
+async function compilePromise(webpackConfig) {
+  const compiler = webpack(webpackConfig);
+  return new Promise((resolve, reject) => {
+    compiler.run((err, stats) => {
+      if (stats.hasErrors()) {
+        reject(err);
+      }
+      resolve(stats);
+    });
+  });
+}
+
+module.exports = function plugin(config, args) {
+  return {
+    async bundle({ srcDirectory, destDirectory, log, jsFilePaths }) {
+      let homepage = config.homepage || "";
+      let fallback = config.devOptions?.fallback || "index.html";
+
+      let dom = new JSDOM(
+        fs.readFileSync(path.join(srcDirectory, config.devOptions.fallback))
+      );
+
+      //Find the first local script, assume it's the entrypoint
+      let scriptEl;
+      for (const el of Array.from(
+        dom.window.document.querySelectorAll("script")
+      )) {
+        let src = el.src.trim();
+        if (
+          el.type.trim().toLowerCase() === "module" &&
+          !src.trim().match(/[a-zA-Z+]+:\/\//g)
+        ) {
+          scriptEl = el;
+          break;
+        }
+      }
+      if (!scriptEl) {
+        throw new Error("Can't bundle without script tag in html");
+      }
+      let entryPoint = scriptEl.src;
+
+      //Compile files using webpack
+      let webpackConfig = {
+        resolve: {
+          alias: {
+            "/web_modules": path.join(srcDirectory, "web_modules/"),
+          },
+        },
+        module: {
+          rules: [
+            {
+              test: /\.jsx?$/,
+              exclude: ["/node_modules/"],
+              use: {
+                loader: "babel-loader",
+              },
+            },
+            {
+              test: /\.s?css$/,
+              use: [
+                {
+                  loader: MiniCssExtractPlugin.loader,
+                },
+                {
+                  loader: "css-loader",
+                },
+              ],
+            },
+            {
+              test: /.*/,
+              exclude: [/\.jsx?$/, /\.s?css$/],
+              use: [
+                {
+                  loader: "file-loader",
+                  options: {
+                    name: "assets/[name]-[hash].[ext]",
+                  },
+                },
+              ],
+            },
+          ],
+        },
+        mode: "production",
+        plugins: [
+          //Extract a css file from imported css files
+          new MiniCssExtractPlugin({
+            filename: "css/style-[hash].css",
+          }),
+          //Copy other files to the destination, excluding ones that are no longer useful
+          new CopyPlugin({
+            patterns: [
+              {
+                from: srcDirectory,
+                to: destDirectory,
+                globOptions: {
+                  ignore: [`/${fallback}`, "**/_dist_/**", "**/web_modules/**"],
+                },
+              },
+            ],
+          }),
+        ],
+      };
+
+      const stats = await compilePromise({
+        ...webpackConfig,
+        entry: path.join(srcDirectory, entryPoint),
+        output: {
+          path: destDirectory,
+          filename: "js/bundle-[hash].js",
+        },
+      }).catch((err) => {
+        console.log(err);
+      });
+
+      //Now that webpack is done, modify the html file to point to the newly compiled resources
+      scriptEl.src = path.join(homepage, `/js/bundle-${stats.hash}.js`);
+      let hasCSS = Object.keys(stats.compilation.assets).some((d) =>
+        d.endsWith(".css")
+      );
+
+      if (hasCSS) {
+        let csslink = dom.window.document.createElement("link");
+        csslink.setAttribute("rel", "stylesheet");
+        csslink.href = path.join(homepage, `/css/style-${stats.hash}.css`);
+        dom.window.document.querySelector("head").append(csslink);
+      }
+
+      //And write our modified html file out to the destination
+      fs.writeFileSync(path.join(destDirectory, fallback), dom.serialize());
+    },
+  };
+};
