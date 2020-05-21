22ff6aa506e7ccba0e0c956779a3670da73c6e82
Fred K. Schott <fkschott@gmail.com>
2020-05-21 08:27:42 -0700

    add hmr to react, svelte, vue templates (#61)

--- packages/create-snowpack-app/templates/app-template-react-typescript/src/index.tsx
+++ packages/create-snowpack-app/templates/app-template-react-typescript/src/index.tsx
@@ -9,3 +9,9 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root'),
 );
+
+// Hot Module Replacement (HMR) - Remove this snippet to remove HMR.
+// Learn more: https://www.snowpack.dev/#hot-module-replacement
+if (import.meta.hot) {
+  import.meta.hot.accept();
+}
--- packages/create-snowpack-app/templates/app-template-svelte/src/index.js
+++ packages/create-snowpack-app/templates/app-template-svelte/src/index.js
@@ -5,3 +5,12 @@ var app = new App({
 });
 
 export default app;
+
+// Hot Module Replacement (HMR) - Remove this snippet to remove HMR.
+// Learn more: https://www.snowpack.dev/#hot-module-replacement
+if (import.meta.hot) {
+  import.meta.hot.accept();
+  import.meta.hot.dispose(() => {
+    app.$destroy();
+  });
+}
