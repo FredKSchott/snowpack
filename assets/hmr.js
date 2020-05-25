444f016c94777a9cf15da9e3ad6a08a1427798be
Fred K. Schott <fkschott@gmail.com>
2020-05-25 00:55:29 -0700

    update to latest esm-hmr snapshot

--- assets/hmr.js
+++ assets/hmr.js
@@ -1,20 +1,14 @@
+/**
+ * esm-hmr/runtime.ts
+ * A client-side implementation of the ESM-HMR spec, for reference.
+ */
 function debug(...args) {
-  console.log('[snowpack:hmr]', ...args);
+  console.log('[ESM-HMR]', ...args);
 }
-
 function reload() {
   location.reload(true);
 }
-
-const REGISTERED_MODULES = {};
 let SOCKET_MESSAGE_QUEUE = [];
-
-const socket = new WebSocket('ws://localhost:12321/');
-socket.addEventListener('open', () => {
-  SOCKET_MESSAGE_QUEUE.forEach(_sendSocketMessage);
-  SOCKET_MESSAGE_QUEUE = [];
-});
-
 function _sendSocketMessage(msg) {
   socket.send(JSON.stringify(msg));
 }
@@ -25,7 +19,12 @@ function sendSocketMessage(msg) {
     _sendSocketMessage(msg);
   }
 }
-
+const socket = new WebSocket('ws://localhost:12321/');
+socket.addEventListener('open', () => {
+  SOCKET_MESSAGE_QUEUE.forEach(_sendSocketMessage);
+  SOCKET_MESSAGE_QUEUE = [];
+});
+const REGISTERED_MODULES = {};
 class HotModuleState {
   constructor(id) {
     this.isLocked = false;
@@ -47,7 +46,7 @@ class HotModuleState {
   decline() {
     this.isDeclined = true;
   }
-  accept(callback = true) {
+  accept(_deps, callback = true) {
     if (this.isLocked) {
       return;
     }
@@ -55,10 +54,28 @@ class HotModuleState {
       sendSocketMessage({id: this.id, type: 'hotAccept'});
       this.isAccepted = true;
     }
-    this.acceptCallbacks.push(callback);
+    if (!Array.isArray(_deps)) {
+      callback = _deps || callback;
+      _deps = [];
+    }
+    if (callback === true) {
+      callback = () => {};
+    }
+    const deps = _deps.map((dep) => {
+      const ext = dep.split('.').pop();
+      if (!ext) {
+        dep += '.js';
+      } else if (ext !== 'js') {
+        dep += '.proxy.js';
+      }
+      return new URL(dep, `${window.location.origin}${this.id}`).pathname;
+    });
+    this.acceptCallbacks.push({
+      deps,
+      callback,
+    });
   }
 }
-
 export function createHotContext(fullUrl) {
   const id = new URL(fullUrl).pathname;
   const existing = REGISTERED_MODULES[id];
@@ -70,9 +87,8 @@ export function createHotContext(fullUrl) {
   REGISTERED_MODULES[id] = state;
   return state;
 }
-
 async function applyUpdate(id) {
-  const state = REGISTERED_MODULES[id] || REGISTERED_MODULES[id + '.proxy.js'];
+  const state = REGISTERED_MODULES[id];
   if (!state) {
     return false;
   }
@@ -84,19 +100,16 @@ async function applyUpdate(id) {
   const disposeCallbacks = state.disposeCallbacks;
   state.disposeCallbacks = [];
   disposeCallbacks.map((callback) => callback({data}));
-  if (acceptCallbacks.length > 0) {
-    const module = await import(id + `?mtime=${Date.now()}`);
-    acceptCallbacks.forEach((callback) => {
-      if (callback === true) {
-        // Do nothing, importing the module side-effects was enough.
-      } else {
-        callback({module, data});
-      }
-    });
+  const updateID = Date.now();
+  for (const {deps, callback: acceptCallback} of acceptCallbacks) {
+    const [module, ...depModules] = await Promise.all([
+      import(id + `?mtime=${updateID}`),
+      ...deps.map((d) => import(d + `?mtime=${updateID}`)),
+    ]);
+    acceptCallback({module, deps: depModules, data});
   }
   return true;
 }
-
 socket.addEventListener('message', ({data: _data}) => {
   if (!_data) {
     return;
@@ -125,5 +138,4 @@ socket.addEventListener('message', ({data: _data}) => {
       reload();
     });
 });
-
 debug('listening for file changes...');
