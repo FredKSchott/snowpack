615bcaa4e85d55106b83b35b38f1bfc4a990c013
Chris Haynes <chris@lamplightdev.com>
2020-05-27 17:13:37 +0100

    Enable the dev server to proxy requests with methods other than GET
    Currently all proxied requests are passed through as GET since no method is specified. This commit updates the proxied call to pass through the method that the initial request was made with.

--- src/commands/dev.ts
+++ src/commands/dev.ts
@@ -371,6 +371,7 @@ export async function command(commandOptions: CommandOptions) {
           const newPath = reqPath.substr(workerConfig.args.toUrl.length);
           try {
             const response = await got(`${workerConfig.args.fromUrl}${newPath}`, {
+              method: req.method,
               headers: req.headers,
               throwHttpErrors: false,
             });
