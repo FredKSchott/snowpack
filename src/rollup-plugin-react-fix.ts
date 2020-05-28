76e2ee67d4455426180b0a0cfb95b5de52c8c9a9
Fred K. Schott <fkschott@gmail.com>
2020-05-28 15:01:07 -0700

    add react-dom fix for __moduleExports

--- src/rollup-plugin-react-fix.ts
+++ src/rollup-plugin-react-fix.ts
@@ -15,6 +15,9 @@ export function rollupPluginReactFix() {
       if (id.endsWith(path.join('react', 'index.js')) && !code.includes('as __moduleExports')) {
         return code + `\nexport { react as __moduleExports };`;
       }
+      if (id.endsWith(path.join('react-dom', 'index.js')) && !code.includes('as __moduleExports')) {
+        return code + `\nexport { reactDom as __moduleExports };`;
+      }
     },
   } as Plugin;
 }
