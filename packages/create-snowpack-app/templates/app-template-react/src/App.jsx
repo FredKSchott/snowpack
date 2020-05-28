6c2f58175e54f806097513753e8c178e84bc43de
Steven Petryk <petryk.steven@gmail.com>
2020-05-27 17:05:46 -0700

    Remove code that said it's supposed to be removed (#76)
    :)

--- packages/create-snowpack-app/templates/app-template-react/src/App.jsx
+++ packages/create-snowpack-app/templates/app-template-react/src/App.jsx
@@ -3,15 +3,12 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  // TODO: Remove before merging, this was just for fast-refresh testing
-  const [counter, setCounter] = useState(5);
-  setTimeout(() => setCounter(counter + 1), 1000);
   return (
     <div className={'App'}>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          {counter} Edit <code>src/App.jsx</code> and save to reload.
+          Edit <code>src/App.jsx</code> and save to reload.
         </p>
         <a
           className="App-link"
