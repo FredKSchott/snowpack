2c6f856eb44f16c60e2fa140c7a55dc7ba11d3a9
Michael Stramel <stramel@users.noreply.github.com>
2020-05-31 00:45:52 -0500

    Add webp, gif, and bmp formats (#98)

--- packages/create-snowpack-app/templates/app-template-react-typescript/types/static.d.ts
+++ packages/create-snowpack-app/templates/app-template-react-typescript/types/static.d.ts
@@ -6,7 +6,11 @@ declare module '*.svg' {
   const ref: string;
   export default ref;
 }
-declare module '*.png' {
+declare module '*.bmp' {
+  const ref: string;
+  export default ref;
+}
+declare module '*.gif' {
   const ref: string;
   export default ref;
 }
@@ -15,6 +19,14 @@ declare module '*.jpg' {
   export default ref;
 }
 declare module '*.jpeg' {
+  const ref: string;
+  export default ref;
+}
+declare module '*.png' {
+  const ref: string;
+  export default ref;
+}
+declare module '*.webp' {
   const ref: string;
   export default ref;
 }
