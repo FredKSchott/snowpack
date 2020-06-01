1b7cc825aa259702e82959e6ccdaa50dc8a64a9b
Fred K. Schott <fkschott@gmail.com>
2020-06-01 15:58:53 -0700

    dotenv: don't throw if NODE_ENV is not set

--- packages/create-snowpack-app/packages/plugin-dotenv/plugin.js
+++ packages/create-snowpack-app/packages/plugin-dotenv/plugin.js
@@ -2,20 +2,15 @@ const fs = require("fs");
 
 module.exports = function plugin(config, options) {
   const NODE_ENV = process.env.NODE_ENV;
-  if (!NODE_ENV) {
-    throw new Error(
-      "The NODE_ENV environment variable is required but was not specified."
-    );
-  }
 
   // https://github.com/bkeepers/dotenv#what-other-env-files-can-i-use
   const dotenvFiles = [
-    `.env.${NODE_ENV}.local`,
+    NODE_ENV && `.env.${NODE_ENV}.local`,
     // Don't include `.env.local` for `test` environment
     // since normally you expect tests to produce the same
     // results for everyone
     NODE_ENV !== "test" && `.env.local`,
-    `.env.${NODE_ENV}`,
+    NODE_ENV && `.env.${NODE_ENV}`,
     ".env",
   ].filter(Boolean);
 
