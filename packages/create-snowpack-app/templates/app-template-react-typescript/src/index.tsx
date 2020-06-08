82df9cd2906c99162c0d3e7874154a886fbf3a8b
Fred K. Schott <fkschott@gmail.com>
2020-06-08 16:48:41 -0700

    update react imports in ts app

--- packages/create-snowpack-app/templates/app-template-react-typescript/src/index.tsx
+++ packages/create-snowpack-app/templates/app-template-react-typescript/src/index.tsx
@@ -1,5 +1,5 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
