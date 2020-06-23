ab08780c94a024f362f2f7e0acdb8733184a546c
jihchi <achi@987.tw>
2020-06-23 21:25:12 +0800

    Add a test case for trailing slash on a path of folder

--- test/build/resolve-imports/src/index.js
+++ test/build/resolve-imports/src/index.js
@@ -10,12 +10,13 @@ console.log(sort, sort_, sort__);
 
 // Importing a directory index.js file
 import components from './components'; // relative import 
+import components______ from './components/'; // relative import with trailing slash
 import components_ from './components/index'; // relative import with index appended
 import components__ from './components/index.js'; // relative import with index appended
 import components___ from 'src/components'; // bare import using mount
 import components____ from 'src/components/index'; // bare import using mount and index appended
 import components_____ from 'src/components/index.js'; // bare import using mount and index.js appended
-console.log(components, components_, components__, components___, components____, components_____);
+console.log(components, components_, components__, components___, components____, components_____, components______);
 
 
 // Importing something that isn't JS
