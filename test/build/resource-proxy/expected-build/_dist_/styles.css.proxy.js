2999d6460c586ae191da11457f668457ce916352
Drew Powers <1369770+drwpow@users.noreply.github.com>
2020-07-01 13:18:25 -0600

    Add resource proxy test (#565)

---
+++ test/build/resource-proxy/expected-build/_dist_/styles.css.proxy.js
@@ -0,0 +1,9 @@
+
+const code = "body {\n  font-family: fantasy;\n}\n";
+
+const styleEl = document.createElement("style");
+const codeEl = document.createTextNode(code);
+styleEl.type = 'text/css';
+
+styleEl.appendChild(codeEl);
+document.head.appendChild(styleEl);
