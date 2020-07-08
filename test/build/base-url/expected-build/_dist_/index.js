4bfb77ef701324e05c0462f70a37a10eb0fb00e8
jonaslindebros <jonaslindebros@gmail.com>
2020-07-08 03:14:48 +0200

    patch: adjusted test for upcomming changes

--- test/build/base-url/expected-build/_dist_/index.js
+++ test/build/base-url/expected-build/_dist_/index.js
@@ -1,3 +1,12 @@
+import __SNOWPACK_ENV__ from '/static/__snowpack__/env.js';
+import.meta.env = __SNOWPACK_ENV__;
+
 import {flatten} from "/static/web_modules/array-flatten.js";
+
+const {MODE} = import.meta.env;
+export function consoleMode() {
+  console.log(MODE);
+}
+
 export default function doNothing() {
 }
--- test/build/base-url/src/index.js
+++ test/build/base-url/src/index.js
@@ -1,5 +1,10 @@
 import {flatten} from 'array-flatten';
 
+const {MODE} = import.meta.env;
+export function consoleMode() {
+  console.log(MODE);
+}
+
 export default function doNothing() {
   // I do nothing 🎉
 }
