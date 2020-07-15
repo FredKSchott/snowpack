31c768296b00313722b15377826dd245b2f7f9ca
Drew Powers <1369770+drwpow@users.noreply.github.com>
2020-07-15 10:32:52 -0600

    Add static asset types (#178)

--- packages/create-snowpack-app/templates/app-template-11ty/.eleventy.js
+++ packages/create-snowpack-app/templates/app-template-11ty/.eleventy.js
@@ -1,23 +1,25 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.setTemplateFormats([
     // Templates:
-    "html",
-    "njk",
-    "md",
+    'html',
+    'njk',
+    'md',
     // Static Assets:
-    "css",
-    "svg",
-    "png",
-    "jpg",
-    "jpeg",
+    'css',
+    'jpeg',
+    'jpg',
+    'png',
+    'svg',
+    'woff',
+    'woff2',
   ]);
-  eleventyConfig.addPassthroughCopy("static");
+  eleventyConfig.addPassthroughCopy('static');
 
   return {
     dir: {
-      input: "_template",
-      includes: "../_includes",
-      output: "_output",
+      input: '_template',
+      includes: '../_includes',
+      output: '_output',
     },
   };
 };
