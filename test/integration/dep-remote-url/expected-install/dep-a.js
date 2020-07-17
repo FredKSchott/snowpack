f51e9bad1ed01ea89fe6c6d7e6407b9c397bc662
Fred K. Schott <fkschott@gmail.com>
2020-07-16 21:24:43 -0700

    add test

---
+++ test/integration/dep-remote-url/expected-install/dep-a.js
@@ -0,0 +1,1 @@
+import a from"http://example.com/FAKE_IMPORT_URL";export*from"https://example.com/FAKE_IMPORT_URL";console.log(a);const b="bar";export{b as foo};
---
+++ test/integration/dep-remote-url/node_modules/dep-a/index.js
@@ -0,0 +1,5 @@
+import pkg from 'http://example.com/FAKE_IMPORT_URL';
+console.log(pkg);
+
+export * from 'https://example.com/FAKE_IMPORT_URL';
+export const foo = 'bar';
