d2789ff1327e272f2f5bc8bad2559b950fc8ab27
Fred K. Schott <fkschott@gmail.com>
2020-07-27 13:46:16 -0700

    add import proxy hook (#632)

---
+++ test/build/import-proxy/expected-build/_dist_/index.js
@@ -0,0 +1,5 @@
+import './styles.css.proxy.js';
+import './logo.png.proxy.js';
+import iconComponent from './icon.svg.proxy.js';
+
+console.log('loaded');
---
+++ test/build/import-proxy/expected-build/_dist_/logo.png.proxy.js
@@ -0,0 +1,1 @@
+export default "/_dist_/logo.png";
