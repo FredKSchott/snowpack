524be916cd974988037ab88b2664506f999f7212
Fred K. Schott <fkschott@gmail.com>
2020-07-28 13:49:34 -0700

    add svelte plugin install handling (#650)

--- packages/@snowpack/plugin-svelte/plugin.js
+++ packages/@snowpack/plugin-svelte/plugin.js
@@ -1,29 +1,33 @@
-const svelte = require("svelte/compiler");
-const fs = require("fs");
-const path = require("path");
+const svelte = require('svelte/compiler');
+const svelteRollupPlugin = require('rollup-plugin-svelte');
+const fs = require('fs');
+const path = require('path');
 
 module.exports = function plugin(config, pluginOptions) {
+  // Support importing Svelte files when you install dependencies.
+  config.installOptions.rollup.plugins.push(svelteRollupPlugin({include: '**/node_modules/**'}));
+
   let svelteOptions;
   let preprocessOptions;
-  const userSvelteConfigLoc = path.join(process.cwd(), "svelte.config.js");
+  const userSvelteConfigLoc = path.join(process.cwd(), 'svelte.config.js');
   if (fs.existsSync(userSvelteConfigLoc)) {
     const userSvelteConfig = require(userSvelteConfigLoc);
-    const { preprocess, ..._svelteOptions } = userSvelteConfig;
+    const {preprocess, ..._svelteOptions} = userSvelteConfig;
     preprocessOptions = preprocess;
     svelteOptions = _svelteOptions;
   }
   // Generate svelte options from user provided config (if given)
   svelteOptions = {
-    dev: process.env.NODE_ENV !== "production",
+    dev: process.env.NODE_ENV !== 'production',
     css: false,
     ...svelteOptions,
     ...pluginOptions,
   };
 
   return {
-    defaultBuildScript: "build:svelte",
-    knownEntrypoints: ["svelte/internal"],
-    async build({ contents, filePath }) {
+    defaultBuildScript: 'build:svelte',
+    knownEntrypoints: ['svelte/internal'],
+    async build({contents, filePath}) {
       let codeToCompile = contents;
       // PRE-PROCESS
       if (preprocessOptions) {
@@ -34,13 +38,13 @@ module.exports = function plugin(config, pluginOptions) {
         ).code;
       }
       // COMPILE
-      const { js, css } = svelte.compile(codeToCompile, {
+      const {js, css} = svelte.compile(codeToCompile, {
         ...svelteOptions,
         filename: filePath,
       });
-      const result = { result: js && js.code };
+      const result = {result: js && js.code};
       if (!svelteOptions.css) {
-        result.resources = { css: css && css.code };
+        result.resources = {css: css && css.code};
       }
       return result;
     },
