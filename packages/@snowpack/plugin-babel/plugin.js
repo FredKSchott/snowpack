9ce6c172b7e7ccdb3fa0d0fda4a4afeae77a72c3
Fred K. Schott <fkschott@gmail.com>
2020-07-27 19:45:52 -0700

    handle process.env in babel plugins

--- packages/@snowpack/plugin-babel/plugin.js
+++ packages/@snowpack/plugin-babel/plugin.js
@@ -10,8 +10,15 @@ module.exports = function plugin(config, options) {
         ast: false,
         compact: false,
       });
-
-      return { result: result.code };
+      let code = result.code;
+      if (code) {
+        // Some Babel plugins assume process.env exists, but Snowpack
+        // uses import.meta.env instead. Handle this here since it
+        // seems to be pretty common.
+        // See: https://www.pika.dev/npm/snowpack/discuss/496
+        code = code.replace(/process\.env/g, 'import.meta.env');
+      }
+      return { result: code };
     },
   };
 };
