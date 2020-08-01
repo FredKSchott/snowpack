d97ffa9b4276de29904508576f2f977b5dc152dc
Michael Lawson <mdlawson@gmx.com>
2020-08-01 17:47:47 +0100

    Add additional error logging in dev server
    Currently errors which occur while building a file are silently swallowed within the dev server, which makes them slightly harder to track down.

--- packages/snowpack/src/commands/dev.ts
+++ packages/snowpack/src/commands/dev.ts
@@ -727,6 +727,7 @@ export async function command(commandOptions: CommandOptions) {
     try {
       responseOutput = await buildFile(fileLoc);
     } catch (err) {
+      console.error(reqPath, err);
       sendError(req, res, 500);
       return;
     }
