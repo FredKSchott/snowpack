4fc721e96c0edbd4826dc67f3c50f94b7b2479a7
Cheng Liu <liucheng.tech@outlook.com>
2020-08-12 11:37:25 +0800

    fix esm dynamic import regex

--- packages/snowpack/src/scan-imports.ts
+++ packages/snowpack/src/scan-imports.ts
@@ -20,7 +20,7 @@ const logger = createLogger({name: 'snowpack'});
 const BARE_SPECIFIER_REGEX = /^[@\w](?!.*(:\/\/))/;
 
 const ESM_IMPORT_REGEX = /import(?:["'\s]*([\w*${}\n\r\t, ]+)\s*from\s*)?\s*["'](.*?)["']/gm;
-const ESM_DYNAMIC_IMPORT_REGEX = /import\((?:['"].+['"]|`[^$]+`)\)/gm;
+const ESM_DYNAMIC_IMPORT_REGEX = /(?<!\.)\bimport\((?:['"].+['"]|`[^$]+`)\)/gm;
 const HAS_NAMED_IMPORTS_REGEX = /^[\w\s\,]*\{(.*)\}/s;
 const STRIP_AS = /\s+as\s+.*/; // for `import { foo as bar }`, strips “as bar”
 const DEFAULT_IMPORT_REGEX = /import\s+(\w)+(,\s\{[\w\s]*\})?\s+from/s;
