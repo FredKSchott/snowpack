ff1ddc692aa98beab244f5c227db4fab7c8a7b08
Luke Edwards <luke.edwards05@gmail.com>
2020-08-27 15:20:29 -0700

    feat(csa): add `--no-install` option

--- test/create-snowpack-app/create-snowpack-app.test.js
+++ test/create-snowpack-app/create-snowpack-app.test.js
@@ -38,6 +38,41 @@ describe('create-snowpack-app', () => {
     // install, since it’s added at the end.
     const snowpackConfigExists = fs.existsSync(path.join(installDir, 'snowpack.config.json'));
     expect(snowpackConfigExists).toBe(true);
+
+    // install node_modules by default
+    const modulesExist = fs.existsSync(path.join(installDir, 'node_modules'));
+    expect(modulesExist).toBe(true);
+  });
+
+  // test `--no-install` option
+  it('npx create-snowpack-app --no-install', () => {
+    const template = 'app-template-preact'; // any template will do
+    const installDir = path.resolve(__dirname, 'test-install');
+
+    rimraf.sync(installDir);
+
+    // run the local create-snowpack-app bin
+    execa.sync(
+      'node',
+      [
+        './create-snowpack-app/cli',
+        `./test/create-snowpack-app/test-install`,
+        '--template',
+        `./create-snowpack-app/${template}`,
+        '--use-yarn', // we use Yarn for this repo
+        '--no-install'
+      ],
+      {cwd: path.resolve(__dirname, '..', '..')},
+    );
+
+    // snowpack.config.json is a file we can test for to assume successful
+    // install, since it’s added at the end.
+    const snowpackConfigExists = fs.existsSync(path.join(installDir, 'snowpack.config.json'));
+    expect(snowpackConfigExists).toBe(true);
+
+    // install node_modules by default
+    const modulesExist = fs.existsSync(path.join(installDir, 'node_modules'));
+    expect(modulesExist).toBe(false);
   });
 
   // template snapshots
