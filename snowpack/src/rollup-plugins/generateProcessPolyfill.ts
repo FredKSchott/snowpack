afa17bd1cb36fdc458c002e408198e13a75731bd
Fred K. Schott <fkschott@gmail.com>
2020-09-04 09:23:55 -0700

    Update generateProcessPolyfill.ts

--- snowpack/src/rollup-plugins/generateProcessPolyfill.ts
+++ snowpack/src/rollup-plugins/generateProcessPolyfill.ts
@@ -23,6 +23,12 @@ TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
+/*
+THIS IS A MODIFIED VERSION OF https://github.com/calvinmetcalf/node-process-es6
+ORIGIANL ADDED IN COMMIT 6304406e065f356aeaa623a878d02be419b316d8 (good to know for diffing) 
+*/
+
+
 export function generateProcessPolyfill(env) {
   return `/* SNOWPACK PROCESS POLYFILL (based on https://github.com/calvinmetcalf/node-process-es6) */
 function defaultSetTimout() {
