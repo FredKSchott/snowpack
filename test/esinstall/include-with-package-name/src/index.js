8f720bc14d7b812dde20b9cd8ed931acf4bc74e6
Gregor Martynus <gregor@martynus.net>
2020-09-17 09:37:53 -0700

    style: prettier

--- test/esinstall/include-with-package-name/src/index.js
+++ test/esinstall/include-with-package-name/src/index.js
@@ -11,7 +11,7 @@ flatten([1, [2, [3, [4, [5], 6], 7], 8], 9]);
 Vue.use(VueRouter);
 
 // test 4: dynamic export
-import('http-vue-loader/src/httpVueLoader.js').then(httpVueLoader => {
+import('http-vue-loader/src/httpVueLoader.js').then((httpVueLoader) => {
   Vue.use(httpVueLoader);
 
   new Vue({
