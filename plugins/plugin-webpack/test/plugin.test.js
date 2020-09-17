2b899e29504d0c5d0ce0edcf3bb60e23e1cad4d6
Gregor Martynus <gregor@martynus.net>
2020-09-17 11:08:59 -0700

    test(@snowpack/plugin-webpack): use describe/it
    resolves https://github.com/pikapkg/snowpack/pull/1061\#discussion_r489707178 /cc @drwpow

--- plugins/plugin-webpack/test/plugin.test.js
+++ plugins/plugin-webpack/test/plugin.test.js
@@ -3,27 +3,31 @@ const fs = require('fs');
 
 const plugin = require('../plugin');
 
-test('minimal - no options', async () => {
-  const pluginInstance = plugin({
-    buildOptions: {},
-  });
+describe('@snowpack/plugin-webpack', () => {
+  it('minimal - no options', async () => {
+    const pluginInstance = plugin({
+      buildOptions: {},
+    });
 
-  fs.writeFileSync = jest.fn();
-  fs.writeFile = jest.fn().mockImplementation((...args) => {
-    const callback = args.pop();
-    callback();
-  });
-  console.log = jest.fn();
+    fs.writeFileSync = jest.fn();
+    fs.writeFile = jest.fn().mockImplementation((...args) => {
+      const callback = args.pop();
+      callback();
+    });
+    console.log = jest.fn();
 
-  await pluginInstance.optimize({
-    buildDirectory: path.resolve(__dirname, 'stubs/minimal/'),
-  });
+    await pluginInstance.optimize({
+      buildDirectory: path.resolve(__dirname, 'stubs/minimal/'),
+    });
 
-  expect(fs.writeFileSync.mock.calls.map(toPathAndStringContent)).toMatchSnapshot(
-    'fs.writeFileSync calls',
-  );
-  expect(fs.writeFile.mock.calls.map(toPathAndStringContent)).toMatchSnapshot('fs.writeFile calls');
-  expect(console.log).toMatchSnapshot('console.log calls');
+    expect(fs.writeFileSync.mock.calls.map(toPathAndStringContent)).toMatchSnapshot(
+      'fs.writeFileSync calls',
+    );
+    expect(fs.writeFile.mock.calls.map(toPathAndStringContent)).toMatchSnapshot(
+      'fs.writeFile calls',
+    );
+    expect(console.log).toMatchSnapshot('console.log calls');
+  });
 });
 
 function toPathAndStringContent([path, content]) {
