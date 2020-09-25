1701956e217e5848cc9a30da2e1c0c97d478f869
Gregor Martynus <gregor@martynus.net>
2020-09-25 12:53:17 -0700

    test(plugin-webpack): use custom jest snapshot serializer

--- plugins/plugin-webpack/test/plugin.test.js
+++ plugins/plugin-webpack/test/plugin.test.js
@@ -1,10 +1,71 @@
 const path = require('path');
 const fs = require('fs');
+const {format} = require('util');
 
+const strpAnsi = require('strip-ansi');
 const plugin = require('../plugin');
+
+/**
+ * Serializer of written files as well as console.log output.
+ *
+ * Both file contents and log output is normalized to account for differences
+ * in UNIX and Windows systems.
+ *
+ * @todo move serializer into seperate file and make it reusable by other tests
+ */
+const snowpackPluginOutputSnapshotSerializer = {
+  serialize({mock}) {
+    const firstCallArg = mock.calls[0][0];
+
+    if (firstCallArg.startsWith(__dirname)) {
+      const calls = mock.calls.filter(isLocal).map(toPathAndStringContent);
+      return calls
+        .map(([path, content]) => {
+          return `FILE: ${path}\n${content}`;
+        })
+        .join(
+          '\n\n--------------------------------------------------------------------------------\n\n',
+        );
+    }
+
+    const outputs = mock.calls.map(toSingleArgument).map(toNoralizedByteSize).map(removeColors);
+
+    return outputs.join('\n');
+  },
+
+  test(value) {
+    return value && value.mock;
+  },
+};
+
+function toPathAndStringContent([path, content]) {
+  const shortPath = path.replace(__dirname, '').substr(1);
+  // unix-ify folder separators for Windows
+  const normalizedPath = shortPath.replace(/\\/g, '/');
+  // unix-ify new lines
+  const normalizedContent = content.toString().replace(/(\\r\\n)/g, '\\n');
+  return [normalizedPath, normalizedContent];
+}
+
+function toSingleArgument([output, ...args]) {
+  return format(output, ...args);
+}
+function toNoralizedByteSize(output) {
+  return output.replace(/(\s{2,})\d+ bytes/g, '$1XXX bytes');
+}
+
+function removeColors(output) {
+  return strpAnsi(output);
+}
+
+function isLocal(mock) {
+  return mock[0].startsWith(__dirname);
+}
 
 describe('@snowpack/plugin-webpack', () => {
   it('minimal - no options', async () => {
+    expect.addSnapshotSerializer(snowpackPluginOutputSnapshotSerializer);
+
     const pluginInstance = plugin({
       buildOptions: {},
     });
@@ -36,16 +97,14 @@ describe('@snowpack/plugin-webpack', () => {
       buildDirectory: path.resolve(__dirname, 'stubs/minimal/'),
     });
 
-    expect(fs.writeFileSync.mock.calls.filter(isLocal).map(toPathAndStringContent)).toMatchSnapshot(
-      'fs.writeFileSync calls',
-    );
-    expect(fs.writeFile.mock.calls.filter(isLocal).map(toPathAndStringContent)).toMatchSnapshot(
-      'fs.writeFile calls',
-    );
+    expect(fs.writeFileSync).toMatchSnapshot('fs.writeFileSync calls');
+    expect(fs.writeFile).toMatchSnapshot('fs.writeFile calls');
     expect(console.log).toMatchSnapshot('console.log calls');
   });
 
   it('minimal - all options', async () => {
+    expect.addSnapshotSerializer(snowpackPluginOutputSnapshotSerializer);
+
     const pluginInstance = plugin(
       {
         buildOptions: {},
@@ -88,32 +147,8 @@ describe('@snowpack/plugin-webpack', () => {
       buildDirectory: path.resolve(__dirname, 'stubs/minimal/'),
     });
 
-    expect(fs.writeFileSync.mock.calls.filter(isLocal).map(toPathAndStringContent)).toMatchSnapshot(
-      'fs.writeFileSync calls',
-    );
-    expect(fs.writeFile.mock.calls.filter(isLocal).map(toPathAndStringContent)).toMatchSnapshot(
-      'fs.writeFile calls',
-    );
-    expect(console.log.mock.calls.map(toNoralizedByteSize)).toMatchSnapshot('console.log calls');
+    expect(fs.writeFileSync).toMatchSnapshot('fs.writeFileSync calls');
+    expect(fs.writeFile).toMatchSnapshot('fs.writeFile calls');
+    expect(console.log).toMatchSnapshot('console.log calls');
   });
 });
-
-function toPathAndStringContent([path, content]) {
-  const shortPath = path.replace(process.cwd(), '');
-  // unix-ify folder separators for Windows
-  const normalizedPath = shortPath.replace(/\\/g, '/');
-  // unix-ify new lines
-  const normalizedContent = content.toString().replace(/(\\r\\n)/g, '\\n');
-  return [normalizedPath, normalizedContent];
-}
-
-function toNoralizedByteSize([output, ...rest]) {
-  return [
-    typeof output === 'string' ? output.replace(/(\s{2,})\d+ bytes/g, '$1XXX bytes') : output,
-    ...rest,
-  ];
-}
-
-function isLocal(mock) {
-  return mock[0].startsWith(__dirname);
-}
