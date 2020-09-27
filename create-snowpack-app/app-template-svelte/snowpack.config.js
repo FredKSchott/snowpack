5163c798ac803b2a39a171f68d6bc5c98695d5f6
Fred K. Schott <fkschott@gmail.com>
2020-09-26 23:48:01 -0700

    update templates to stop using app-scripts for snowpack, babel, tsc config

---
+++ create-snowpack-app/app-template-svelte/snowpack.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  mount: {
+    public: '/',
+    src: '/_dist_',
+  },
+  plugins: [
+    '@snowpack/plugin-svelte',
+    '@snowpack/plugin-dotenv',
+    [
+      '@snowpack/plugin-run-script',
+      {cmd: 'svelte-check --output human', watch: '$1 --watch', output: 'stream'},
+    ],
+  ],
+};
