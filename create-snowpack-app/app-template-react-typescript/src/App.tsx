30469ca56689fdedd0024713eacd8d4fc7cd45e4
Fred K. Schott <fkschott@gmail.com>
2020-10-02 13:00:26 -0700

    add fast refresh example to react/preact templates

--- create-snowpack-app/app-template-react-typescript/src/App.tsx
+++ create-snowpack-app/app-template-react-typescript/src/App.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 interface AppProps {}
 
 function App({}: AppProps) {
+  const [count, setCount] = useState(0);
+  setTimeout(() => setCount(count + 1), 1000);
   return (
     <div className="App">
       <header className="App-header">
@@ -12,14 +14,19 @@ function App({}: AppProps) {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          Page has been open for <code>{count}</code> seconds.
+        </p>
+        <p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </p>
       </header>
     </div>
   );
