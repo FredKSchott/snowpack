0338f8e100dcbc17d945d9c1a6121e02da987586
Fred K. Schott <fkschott@gmail.com>
2020-10-04 17:55:17 -0700

    update docs and templates

--- create-snowpack-app/app-template-svelte-typescript/snowpack.config.js
+++ create-snowpack-app/app-template-svelte-typescript/snowpack.config.js
@@ -6,10 +6,10 @@ module.exports = {
   plugins: [
     '@snowpack/plugin-svelte',
     '@snowpack/plugin-dotenv',
+    "@snowpack/plugin-typescript",
     [
       '@snowpack/plugin-run-script',
       {cmd: 'svelte-check --output human', watch: '$1 --watch', output: 'stream'},
     ],
-    ['@snowpack/plugin-run-script', {cmd: 'tsc --noEmit', watch: '$1 --watch'}],
   ],
 };
