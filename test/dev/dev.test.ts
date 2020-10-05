8e45f6bc693b8ae7ae5d69604e0e67ed6e3dc742
Gregor Martynus <gregor@martynus.net>
2020-10-05 15:37:08 -0700

    cancel spawned process in afterEach

--- test/dev/dev.test.ts
+++ test/dev/dev.test.ts
@@ -7,11 +7,26 @@ const os = require('os');
 const got = require('got');
 
 describe('snowpack dev', () => {
+  let snowpackProcess;
+  afterEach(async () => {
+    snowpackProcess.cancel();
+
+    try {
+      await snowpackProcess;
+    } catch (error) {
+      expect(error.isCanceled).toEqual(true);
+    }
+  });
+
   it('smoke', async () => {
+    expect.assertions(3);
+
     const cwd = path.join(__dirname, 'smoke');
 
     // start the server
-    const snowpackProcess = execa('yarn', ['teststart'], {cwd});
+    // NOTE: we tried spawning `yarn` here, but the process was not cleaned up
+    //       correctly on CI and the action got stuck. npx does not cause that problem.
+    snowpackProcess = execa('npx', ['snowpack', 'dev'], {cwd});
 
     // await server to be ready and set a timeout in case something goes wrong
     await new Promise((resolve, reject) => {
@@ -40,8 +55,5 @@ describe('snowpack dev', () => {
     // get built JS
     const {body: jsBody} = await got('http://localhost:8080/_dist_/index.js');
     expect(jsBody).toMatchSnapshot('js');
-
-    // all set
-    snowpackProcess.cancel();
   });
 });
