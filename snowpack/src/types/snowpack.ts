fd269d782ae6deecfcc80128e771b7f11e49d5ef
Fred K. Schott <fkschott@gmail.com>
2020-10-05 21:19:37 -0700

    update ts

--- snowpack/src/types/snowpack.ts
+++ snowpack/src/types/snowpack.ts
@@ -92,9 +92,9 @@ export interface SnowpackPlugin {
   /** read and modify the Snowpack config object */
   config?(snowpackConfig: SnowpackConfig): void;
   /** Called when a watched file changes during development. */
-  onChange({filePath}: {filePath: string}): void;
+  onChange?({filePath}: {filePath: string}): void;
   /** (internal interface, not set by the user) Mark a file as changed. */
-  markChanged(file: string): void;
+  markChanged?(file: string): void;
 }
 
 export interface LegacySnowpackPlugin {
