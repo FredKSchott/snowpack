cfa6bc40d6c68bddb572ae734c4a48e7869b899a
Fred K. Schott <fkschott@gmail.com>
2020-10-07 17:35:46 -0700

    update add true .sass support

--- plugins/plugin-sass/plugin.js
+++ plugins/plugin-sass/plugin.js
@@ -5,7 +5,7 @@ const npmRunPath = require('npm-run-path');
 
 const IMPORT_REGEX = /\@(use|import)\s*['"](.*?)['"]/g;
 
-function scanSassImports(fileContents, filePath) {
+function scanSassImports(fileContents, filePath, fileExt) {
   // TODO: Replace with matchAll once Node v10 is out of TLS.
   // const allMatches = [...result.matchAll(new RegExp(HTML_JS_REGEX))];
   const allMatches = [];
@@ -14,18 +14,17 @@ function scanSassImports(fileContents, filePath) {
   while ((match = regex.exec(fileContents))) {
     allMatches.push(match);
   }
-  // Inherit the default file extension of the importer. This is a cheap
-  // but effective shortcut to supporting both ".scss" and ".sass"
-  // since users rarely mix both, and performing multiple lookups
-  // would be too expensive.
-  const missingFileExt = path.extname(filePath);
   // return all imports, resolved to true files on disk.
   return allMatches
     .map((match) => match[2])
     .filter((s) => s.trim())
     .map((s) => {
+      // Inherit the default file extension of the importer. This is a cheap
+      // but effective shortcut to supporting both ".scss" and ".sass"
+      // since users rarely mix both, and performing multiple lookups
+      // would be too expensive.
       if (!path.extname(s)) {
-        s += missingFileExt;
+        s += fileExt;
       }
       return path.resolve(path.dirname(filePath), s);
     });
@@ -64,22 +63,24 @@ module.exports = function postcssPlugin(_, {native}) {
     },
     /** Load the Sass file and compile it to CSS. */
     async load({filePath, isDev}) {
+      const fileExt = path.extname(filePath);
       const contents = fs.readFileSync(filePath, 'utf8');
       // During development, we need to track changes to Sass dependencies.
       if (isDev) {
-        const sassImports = scanSassImports(contents, filePath);
+        const sassImports = scanSassImports(contents, filePath, fileExt);
         addImportsToMap(filePath, sassImports);
       }
+      // If file is `.sass`, use YAML-style. Otherwise, use default.
+      const args = ['--stdin', '--load-path', path.dirname(filePath)];
+      if (fileExt === '.sass') {
+        args.push('--indented');
+      }
       // Build the file.
-      const {stdout, stderr} = await execa(
-        'sass',
-        ['--stdin', '--load-path', path.dirname(filePath)],
-        {
-          input: contents,
-          env: native ? undefined : npmRunPath.env(),
-          extendEnv: native ? true : false,
-        },
-      );
+      const {stdout, stderr} = await execa('sass', args, {
+        input: contents,
+        env: native ? undefined : npmRunPath.env(),
+        extendEnv: native ? true : false,
+      });
       // Handle the output.
       if (stderr) throw new Error(stderr);
       if (stdout) return stdout;
