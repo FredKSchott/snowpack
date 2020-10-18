0dfbfbe0ade6fdb3a915d13c1aaca61f74264c4a
Benjamin Kinga <somethingscripted@gmail.com>
2020-10-18 15:16:44 -0500

    initial commit

---
+++ plugins/plugin-postcss/test/plugin.test.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const path = require('path');
+const plugin = require("../plugin.js");
+
+jest.mock('@babel/core')
+const cssnano = require("@babel/core")
+const cssPath = path.resolve(__dirname, './stubs/style.css');
+const cssContent = fs.readFileSync(cssPath, 'utf-8');
+const configFilePath = path.resolve(__dirname, './stubs/postcss.config.js');
+
+describe('@snowpack/plugin-postcss', () => {
+    test('with no options', async () => {
+        const pluginInstance = plugin({}, {});
+        const transformCSSResults = await pluginInstance.transform({
+            fileExt: path.extname(cssPath),
+            contents: cssContent 
+        });
+
+        expect(transformCSSResults).toMatchSnapshot();
+    });
+
+    test('passing in a config file', async () => {
+        const options = {
+            config: path.resolve(configFilePath)
+        }
+        const pluginInstance = plugin({}, options)
+        const transformCSSResults = await pluginInstance.transform({
+            fileExt: path.extname(cssPath),
+            contents: cssContent 
+        });
+        expect(transformCSSResults).toMatchSnapshot();
+    });
+});
---
+++ plugins/plugin-postcss/test/stubs/postcss.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    plugins: [
+        require('cssnano')({
+            preset: 'default',
+        }),
+    ],
+};
