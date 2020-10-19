09afc67c85fea8fc691657d8c999e128ea2f28e8
Rafael Hengles <rhengles@gmail.com>
2020-10-18 21:34:21 -0300

    Fix test for Windows

--- test/build/secondary-extensions/html-template-plugin.js
+++ test/build/secondary-extensions/html-template-plugin.js
@@ -1,5 +1,8 @@
 const fs = require('fs').promises;
 
+const rePathSlash = /([\\\/])[^\\\/]*$/;
+const reAllSlashes = /[\\\/]/g;
+
 module.exports = function (snowConf, plugOpt) {
   return {
     resolve: {
@@ -7,6 +10,10 @@ module.exports = function (snowConf, plugOpt) {
       output: ['.html.js']
     },
     async load({ filePath }) {
+      const slash = filePath.match(rePathSlash);
+      if (slash) {
+        plugOpt.base = plugOpt.base.replace(reAllSlashes, slash[1]);
+      }
       var bpIndex = filePath.indexOf(plugOpt.base);
       if (-1 === bpIndex) return;
       const fileContents = await fs.readFile(filePath, 'utf-8');
