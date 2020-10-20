d90a1fb8a080bfe32e7283d87063381cd97f48bb
gr2m <gr2m@users.noreply.github.com>
2020-10-20 22:58:04 +0000

    [ci] yarn format

--- plugins/plugin-react-refresh/test/stubs/stub.js
+++ plugins/plugin-react-refresh/test/stubs/stub.js
@@ -1,5 +1,5 @@
-import React from "react";
+import React from 'react';
 function App() {
-  return /* @__PURE__ */ React.createElement("div", null, "React App");
+  return /* @__PURE__ */ React.createElement('div', null, 'React App');
 }
 export default App;
