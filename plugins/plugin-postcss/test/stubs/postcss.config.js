d4e548c3cc77bcab17148686502878a656809869
drwpow <drwpow@users.noreply.github.com>
2020-10-27 18:35:44 +0000

    [ci] yarn format

--- plugins/plugin-postcss/test/stubs/postcss.config.js
+++ plugins/plugin-postcss/test/stubs/postcss.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    plugins: [
-        require('cssnano')({
-            preset: 'default',
-        }),
-    ],
+  plugins: [
+    require('cssnano')({
+      preset: 'default',
+    }),
+  ],
 };
