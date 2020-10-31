d1461cb520490ddd6c0f5ad9da9cfe204cbef0db
Fred K. Schott <fkschott@gmail.com>
2020-10-30 23:13:34 -0700

    finish template test runner transition

--- create-snowpack-app/app-template-lit-element/web-test-runner.config.js
+++
@@ -1,6 +0,0 @@
-// NODE_ENV=test - Needed by "@snowpack/web-test-runner-plugin"
-process.env.NODE_ENV = 'test';
-
-module.exports = {
-  plugins: [require('@snowpack/web-test-runner-plugin')()],
-};
