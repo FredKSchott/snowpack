e7ce5f442ced1f99071f535b43e10c7ff69e9555
Fred K. Schott <fkschott@gmail.com>
2020-11-05 15:49:05 -0800

    add init command, remove no command behavior

---
+++ snowpack/assets/snowpack-init-file.js
@@ -0,0 +1,11 @@
+// Snowpack Configuration File
+// See all supported options: https://www.snowpack.dev/#configuration
+
+/** @type {import("snowpack").SnowpackUserConfig } */
+module.exports = {
+  // mount: {},
+  // plugins: [],
+  // installOptions: {},
+  // devOptions: {},
+  // buildOptions: {},
+};
