fec4f57df0e29178f1f7a64a8d0aeeecf4db9412
Fred K. Schott <fkschott@gmail.com>
2020-11-07 22:31:49 -0800

    add skypack package (#1135)

---
+++ skypack/index.esm.mjs
@@ -0,0 +1,9 @@
+import Pkg from './lib/index.js';
+
+export const rollupPluginSkypack = Pkg.rollupPluginSkypack;
+export const generateImportMap = Pkg.generateImportMap;
+export const fetchCDN = Pkg.fetchCDN;
+export const buildNewPackage = Pkg.buildNewPackage;
+export const lookupBySpecifier = Pkg.lookupBySpecifier;
+export const clearCache = Pkg.clearCache;
+export const SKYPACK_ORIGIN = Pkg.SKYPACK_ORIGIN;
