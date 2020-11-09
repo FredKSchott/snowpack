8d77fcb03920ba8e61e8ade1c2cb0a36807494ab
Fred K. Schott <fkschott@gmail.com>
2020-11-09 08:52:24 -0800

    Fix the typing issue by pinning to an older version of the @types package
    This reverts commit 269bf7c8f9e14fc97f2961b1a2e4bf59f77fb394.

--- create-snowpack-app/app-template-svelte-typescript/src/index.ts
+++ create-snowpack-app/app-template-svelte-typescript/src/index.ts
@@ -9,8 +9,8 @@ export default app;
 // Hot Module Replacement (HMR) - Remove this snippet to remove HMR.
 // Learn more: https://www.snowpack.dev/#hot-module-replacement
 if (import.meta.hot) {
-  import.meta.hot!.accept();
-  import.meta.hot!.dispose(() => {
+  import.meta.hot.accept();
+  import.meta.hot.dispose(() => {
     app.$destroy();
   });
 }
