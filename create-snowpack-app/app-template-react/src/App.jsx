79c2f7b455f061c95ec872ec2337651a00f8a2db
markfirmware <markfirmware@users.noreply.github.com>
2020-11-10 20:17:30 +0000

    Update App.jsx

--- create-snowpack-app/app-template-react/src/App.jsx
+++ create-snowpack-app/app-template-react/src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import logo from 'logo.svg';
-import 'App.css';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   // Create the count state.
