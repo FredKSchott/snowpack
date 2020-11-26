3fbf1a256a77374d597704a1b8e62e606b7afe8a
Fred K. Schott <fkschott@gmail.com>
2020-11-25 23:27:52 -0800

    update externalesm support (#1668)

--- test/esinstall/config-external/packages/config-external-pkg-a/entrypoint.js
+++ test/esinstall/config-external/packages/config-external-pkg-a/entrypoint.js
@@ -1,3 +1,4 @@
 import {foo} from 'config-external-pkg-b';
 import {foo as foo_} from 'config-external-pkg-b/entrypoint';
-console.log(foo, foo_);
+import bar from 'config-external-pkg-c';
+console.log(foo, foo_, bar);
---
+++ test/esinstall/config-external/packages/config-external-pkg-cjs/entrypoint.js
@@ -0,0 +1,3 @@
+const foo = require('config-external-pkg-b');
+const _foo = require('config-external-pkg-c');
+console.log(foo, _foo);
