7777dd91adf8275e1bc70806b69d1c2273356981
Matthew Phillips <matthew@skypack.dev>
2020-12-08 10:40:57 -0500

    Move esinstall tests into test folders
    This moves all of the esinstall tests into individual test folders so that jest can run them in parallel. This removes the test/esinstall/install.test.js file as well.

---
+++ test/esinstall/dep-list-complex/dep-list-complex.test.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const {
+  existsPackageJson,
+  runTest,
+  testLockFile,
+  testWebModules,
+} = require('../esinstall-test-utils.js');
+
+require('jest-specific-snapshot'); // allows to call expect().toMatchSpecificSnapshot(filename, snapshotName)
+
+describe('dep-list-complex', () => {
+  beforeAll(() => {
+    // Needed so that ora (spinner) doesn't use platform-specific characters
+    process.env.CI = '1';
+  });
+
+  it('matches the snapshot', async () => {
+    const cwd = __dirname;
+
+    if (existsPackageJson(cwd) === false) return;
+
+    // Run Test
+    const {output, snapshotFile} = await runTest(cwd);
+
+    // Test output
+    expect(output).toMatchSpecificSnapshot(snapshotFile, 'cli output');
+
+    // Test Lockfile (if one exists)
+    await testLockFile(cwd);
+
+    // Cleanup
+    const {testAllSnapshots, testDiffs} = testWebModules(cwd, snapshotFile);
+
+    // Assert that the snapshots match
+    testAllSnapshots();
+
+    // If any diffs are detected, we'll assert the difference so that we get nice output.
+    testDiffs();
+  });
+});
