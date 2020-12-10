1ec10be7d3d0f7d8b4c210fce0704517d8917128
Matthew Phillips <matthew@skypack.dev>
2020-12-10 15:24:12 -0500

    Increase the test timeout

--- rollup/dist/shared/rollup.js
+++ rollup/dist/shared/rollup.js
@@ -17886,7 +17886,7 @@ function writeFile(dest, data) {
         }
 
         console.log("AFTER MKPATH")
-        console.log("FSWRITE", dest, data);
+        console.log("FSWRITE", dest);
         fs.writeFile(dest, data, err => {
             console.log("FILE WRITTEN", err);
             if (err) {
