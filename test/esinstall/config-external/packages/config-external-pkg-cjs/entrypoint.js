b6339acc9d45ce926244517f1d36a05a6cb7c3d0
Drew Powers <1369770+drwpow@users.noreply.github.com>
2020-12-10 20:22:20 -0700

    Delete old esinstall tests (#1907)

--- test/esinstall/config-external/packages/config-external-pkg-cjs/entrypoint.js
+++
@@ -1,3 +0,0 @@
-const foo = require('config-external-pkg-b');
-const _foo = require('config-external-pkg-c');
-console.log(foo, _foo);
--- test/esinstall/include/dir/g.ts
+++
@@ -1,4 +0,0 @@
-// test 7: Ignore babel macros
-import 'twin.macro';
-// @ts-ignore
-import preval from 'preval.macro';
