d4292d7438213653d58e640e4cc5592e9cbcb918
Fred K. Schott <fkschott@gmail.com>
2020-12-13 12:19:09 -0800

    Revert "fix postcss plugin cwd for relative imports"
    This reverts commit a05e56cbf896f3453b45d26392a77b8f953e769b.

--- plugins/plugin-postcss/plugin.js
+++ plugins/plugin-postcss/plugin.js
@@ -1,10 +1,9 @@
-const path = require('path');
 const execa = require('execa');
 
 module.exports = function postcssPlugin(_, options) {
   return {
     name: '@snowpack/postcss-transform',
-    async transform({id, fileExt, contents}) {
+    async transform({fileExt, contents}) {
       const {input = ['.css'], config} = options;
       if (!input.includes(fileExt) || !contents) return;
 
@@ -12,7 +11,7 @@ module.exports = function postcssPlugin(_, options) {
       if (config) flags.push(`--config ${config}`);
 
       const {stdout} = await execa('postcss', flags, {
-        cwd: path.dirname(id),
+        cwd: process.cwd(),
         input: contents,
       });
 
