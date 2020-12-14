b718459eec47565ec7f33964a138d702da2ae51f
Radu Dascalu <r.dascalu@cmcmarkets.com>
2020-12-14 13:34:00 +0000

    Refactor custom-modules-dir test to use JS API

--- test/build/custom-modules-dir/custom-modules-dir.test.js
+++ test/build/custom-modules-dir/custom-modules-dir.test.js
@@ -1,24 +1,38 @@
 const fs = require('fs');
 const path = require('path');
-const {setupBuildTest, readFiles} = require('../../test-utils');
+const snowpack = require('../../../snowpack');
 
-const cwd = path.join(__dirname, 'build');
-let files = {};
+const TEST_ROOT = __dirname;
+const TEST_OUT = path.join(__dirname, 'build');
+let result;
+
+function getFile(id) {
+  return result[path.resolve(TEST_OUT, id)].contents;
+}
 
 describe('buildOptions.webModulesUrl', () => {
-  beforeAll(() => {
-    setupBuildTest(__dirname);
-
-    files = readFiles(cwd);
+  beforeAll(async () => {
+    const [, config] = snowpack.createConfiguration({
+      root: TEST_ROOT,
+      mount: {
+        [path.resolve(TEST_ROOT, './src')]: '/_dist_',
+      },
+      buildOptions: {
+        out: TEST_OUT,
+        webModulesUrl: '/my_modules',
+      },
+    });
+    const {result: _result} = await snowpack.buildProject({config, lockfile: null});
+    result = _result;
   });
 
   it('snowpack: installs in specified directory', () => {
-    const webModulesUrl = path.join(cwd, 'my_modules');
+    const webModulesUrl = path.join(TEST_OUT, 'my_modules');
     expect(fs.existsSync(webModulesUrl)).toBe(true);
   });
 
   it('JS: uses webModulesUrl', () => {
-    expect(files['/_dist_/index.js']).toEqual(
+    expect(getFile('./_dist_/index.js')).toEqual(
       expect.stringContaining(`import {flatten} from '../my_modules/array-flatten.js';`),
     );
   });
