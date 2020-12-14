0df20a15f607c1b8b1427e97fb17d86219ef35ea
Radu Dascalu <r.dascalu@cmcmarkets.com>
2020-12-14 13:44:58 +0000

    Refactor legacy-mount test to use JS API

--- test/build/legacy-mount-scripts/legacy-mount.test.js
+++ test/build/legacy-mount-scripts/legacy-mount.test.js
@@ -1,22 +1,33 @@
 const fs = require('fs');
 const path = require('path');
-const {setupBuildTest} = require('../../test-utils');
+const snowpack = require('../../../snowpack');
 
-const cwd = path.join(__dirname, 'build');
+const TEST_ROOT = __dirname;
+const TEST_OUT = path.join(__dirname, 'build');
 
 describe('config: mount scripts (legacy)', () => {
-  beforeAll(() => {
-    setupBuildTest(__dirname);
+  beforeAll(async () => {
+    const [, config] = snowpack.createConfiguration({
+      root: TEST_ROOT,
+      scripts: {
+        'mount:src': 'mount src --to /_dist_',
+        'mount:public': 'mount public --to /',
+      },
+      buildOptions: {
+        out: TEST_OUT,
+      },
+    });
+    await snowpack.buildProject({config, lockfile: null});
   });
 
   it('mounted ./src', () => {
-    const jsLoc = path.join(cwd, '_dist_', 'index.js');
+    const jsLoc = path.join(TEST_OUT, '_dist_', 'index.js');
     expect(fs.existsSync(jsLoc)).toBe(true); // file exists
     expect(fs.readFileSync(jsLoc, 'utf8')).toBeTruthy(); // file has content
   });
 
   it('mounted ./public', () => {
-    const htmlLoc = path.join(cwd, 'index.html');
+    const htmlLoc = path.join(TEST_OUT, 'index.html');
     expect(fs.existsSync(htmlLoc)).toBe(true); // file exists
     expect(fs.readFileSync(htmlLoc, 'utf8')).toBeTruthy(); // file has content
   });
