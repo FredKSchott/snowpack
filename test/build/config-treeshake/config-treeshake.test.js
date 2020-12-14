771a08a66abe1b39bfa973bebc50b832d27aaeb9
Radu Dascalu <r.dascalu@cmcmarkets.com>
2020-12-14 13:16:13 +0000

    Refactor config-treeshake test to use JS API

--- test/build/config-treeshake/config-treeshake.test.js
+++ test/build/config-treeshake/config-treeshake.test.js
@@ -1,25 +1,38 @@
 const fs = require('fs');
 const path = require('path');
-const {setupBuildTest} = require('../../test-utils');
+const snowpack = require('../../../snowpack');
 
-const cwd = path.join(__dirname, 'build');
+const TEST_ROOT = __dirname;
+const TEST_OUT = path.join(__dirname, 'build');
 
 // Note(drew): full disclosure, I do not know what this test was originally-testing; it was not clear from the snapshot (the snapshot had functions that seemed like they should have been treeshaken)
 
 describe('installOptions.treeshake', () => {
-  beforeAll(() => {
-    setupBuildTest(__dirname);
+  beforeAll(async () => {
+    const [, config] = snowpack.createConfiguration({
+      root: TEST_ROOT,
+      installOptions: {
+        treeshake: true,
+      },
+      mount: {
+        [path.resolve(TEST_ROOT, './src')]: '/_dist_',
+      },
+      buildOptions: {
+        out: TEST_OUT,
+      },
+    });
+    await snowpack.buildProject({config, lockfile: null});
   });
 
   it('built web_modules/array-flatten.js', () => {
-    const webModuleLoc = path.join(cwd, 'web_modules', 'array-flatten.js');
+    const webModuleLoc = path.join(TEST_OUT, 'web_modules', 'array-flatten.js');
 
     expect(fs.existsSync(webModuleLoc)).toBe(true); // file exists
     expect(fs.readFileSync(webModuleLoc, 'utf8')).toBeTruthy(); // file has contents
   });
 
   it('built web_modules/async.js', () => {
-    const webModuleLoc = path.join(cwd, 'web_modules', 'async.js');
+    const webModuleLoc = path.join(TEST_OUT, 'web_modules', 'async.js');
 
     expect(fs.existsSync(webModuleLoc)).toBe(true); // file exists
     expect(fs.readFileSync(webModuleLoc, 'utf8')).toBeTruthy(); // file has contents
