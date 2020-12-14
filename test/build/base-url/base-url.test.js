ce198a6c196981e7d6eb3d9d61dfc0971d5e3213
Fred K. Schott <fkschott@gmail.com>
2020-12-13 21:38:42 -0800

    update test for root behavior

--- test/build/base-url/base-url.test.js
+++ test/build/base-url/base-url.test.js
@@ -1,43 +1,57 @@
 const path = require('path');
 const cheerio = require('cheerio');
-const {setupBuildTest, readFiles} = require('../../test-utils');
+const snowpack = require('../../../snowpack');
 
-const cwd = path.join(__dirname, 'build');
+const TEST_ROOT = __dirname;
+const TEST_OUT = path.join(__dirname, 'build');
+let result;
 
-let files = {};
+function getFile(id) {
+  return result[path.resolve(TEST_OUT, id)].contents;
+}
 
 describe('buildOptions.baseUrl', () => {
-  beforeAll(() => {
-    setupBuildTest(__dirname);
-
-    files = readFiles(cwd);
+  beforeAll(async () => {
+    const [, config] = snowpack.createConfiguration({
+      root: TEST_ROOT,
+      mount: {
+        [path.resolve(TEST_ROOT, './public')]: '/',
+        [path.resolve(TEST_ROOT, './src')]: '/_dist_',
+      },
+      buildOptions: {
+        baseUrl: '/static/',
+        out: TEST_OUT,
+      },
+    });
+    const {result: _result} = await snowpack.buildProject({config, lockfile: null});
+    result = _result;
   });
 
   it('baseUrl works for <link>', () => {
-    const $ = cheerio.load(files['/index.html']);
+    const $ = cheerio.load(getFile('./index.html'));
     expect($('link[rel="icon"]').attr('href').startsWith('/static/')).toBe(true);
     expect($('link[rel="stylesheet"]').attr('href').startsWith('/static/')).toBe(true);
   });
 
   it('baseUrl works for <script>', () => {
-    const $ = cheerio.load(files['/index.html']);
+    const $ = cheerio.load(getFile('./index.html'));
     expect($('script').attr('src').startsWith('/static/')).toBe(true);
   });
 
   it('import.meta.env works', () => {
     // env is present in index.js
-    expect(files['/index.js']).toEqual(
+    expect(getFile('./index.js')).toEqual(
       expect.stringContaining(`import __SNOWPACK_ENV__ from './__snowpack__/env.js';`),
     );
-    expect(files['/index.js']).toEqual(
+    expect(getFile('./index.js')).toEqual(
       expect.stringContaining(`import.meta.env = __SNOWPACK_ENV__;`),
     );
 
     // env is present in _dist_/index.js too
-    expect(files['/_dist_/index.js']).toEqual(
+    expect(getFile('./_dist_/index.js')).toEqual(
       expect.stringContaining(`import __SNOWPACK_ENV__ from '../__snowpack__/env.js';`),
     );
-    expect(files['/_dist_/index.js']).toEqual(
+    expect(getFile('./_dist_/index.js')).toEqual(
       expect.stringContaining(`import.meta.env = __SNOWPACK_ENV__;`),
     );
   });
