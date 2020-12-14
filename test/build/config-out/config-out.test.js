b5bf8bf500f4e881b5d5635f6b5882a69fe4de89
Radu Dascalu <r.dascalu@cmcmarkets.com>
2020-12-14 13:12:50 +0000

    Refactor config-out test to use JS API

--- test/build/config-out/config-out.test.js
+++ test/build/config-out/config-out.test.js
@@ -1,19 +1,30 @@
 const fs = require('fs');
 const path = require('path');
-const {setupBuildTest} = require('../../test-utils');
+const snowpack = require('../../../snowpack');
 
-const cwd = path.join(__dirname, 'TEST_BUILD_OUT');
+const TEST_ROOT = __dirname;
+const TEST_OUT = path.join(__dirname, 'TEST_BUILD_OUT');
 
 describe('config: buildOptions.out', () => {
-  beforeAll(() => {
-    setupBuildTest(__dirname);
+  beforeAll(async () => {
+    const [, config] = snowpack.createConfiguration({
+      root: TEST_ROOT,
+      mount: {
+        [path.resolve(TEST_ROOT, './src')]: '/src',
+      },
+      buildOptions: {
+        clean: true,
+        out: TEST_OUT,
+      },
+    });
+    await snowpack.buildProject({config, lockfile: null});
   });
 
   it('respects buildOptions.out', () => {
-    const distJSLoc = path.join(cwd, 'src', 'index.js');
+    const distJSLoc = path.join(TEST_OUT, 'src', 'index.js');
     expect(fs.existsSync(distJSLoc)).toBe(true); // JS file exists
 
-    const snowpackMetaLoc = path.join(cwd, '__snowpack__', 'env.js');
+    const snowpackMetaLoc = path.join(TEST_OUT, '__snowpack__', 'env.js');
     expect(fs.existsSync(snowpackMetaLoc)).toBe(true); // snowpack meta exists
   });
 });
