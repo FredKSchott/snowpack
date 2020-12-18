4b182b429fda5812d6f96c3a9a428aec417c8457
Matthew Phillips <matthew@matthewphillips.info>
2020-12-18 12:42:01 -0500

    Prevent auto-exporting CJS modules with invalid identifiers (#2035)
    This fixes keyboard-key

---
+++ test/esinstall/cjs-autodetect-exports/cjs-autodetect-exports.test.js
@@ -0,0 +1,44 @@
+const fs = require('fs');
+const path = require('path');
+const {install} = require('../../../esinstall/lib');
+
+// This test simulates what keyboard-key is doing.
+describe('Auto-detecting CJS exports', () => {
+  it('should not attempt to convert package with invalid identifiers as exports', async () => {
+    const cwd = __dirname;
+    const dest = path.join(cwd, 'test-cjs-invalid-exports');
+    const spec = 'cjs-invalid-exports';
+
+    const {
+      importMap: {imports},
+    } = await install([spec], {
+      cwd,
+      dest,
+    });
+
+    const output = fs.readFileSync(path.join(dest, `${spec}.js`), 'utf8');
+    expect(output).toEqual(
+      // This shouldn't contain named exports
+      expect.not.stringContaining(`export {`),
+    );
+  });
+
+  it('should convert package with valid identifiers as exports', async () => {
+    const cwd = __dirname;
+    const dest = path.join(cwd, 'test-cjs-valid-exports');
+    const spec = 'cjs-valid-exports';
+
+    const {
+      importMap: {imports},
+    } = await install([spec], {
+      cwd,
+      dest,
+    });
+
+    const output = fs.readFileSync(path.join(dest, `${spec}.js`), 'utf8');
+    expect(output).toEqual(
+      // Correctly exports the valid identifiers as tree-shakeable identifiers
+      expect.stringContaining(`export { entrypoint as __moduleExports, a, b, d };`),
+    );
+  });
+});
