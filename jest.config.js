685d667bd1a53367a4d9140383e5796ce4d26fc3
TruongSinh Tran-Nguyen <i@truongsinh.pro>
2020-12-20 13:12:44 -0600

    different way to exlude untransformed test file

--- jest.config.js
+++ jest.config.js
@@ -2,10 +2,10 @@ module.exports = {
   modulePathIgnorePatterns: [
     '<rootDir>/create-snowpack-app/app-template-', // don’t run tests intended as user examples
     '<rootDir>/examples', // don’t run any tests in examples
+    '<rootDir>/snowpack/src', // `snowpack/src` is not yet transformed, run `snowpack/lib` instead
     '<rootDir>/test/create-snowpack-app/test-install', // don’t run tests inside our mock create-snowpack-app install
     '<rootDir>/www', // docs has its own tests
   ],
-  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.js$', // `tsx?` and `jsx` files are not transformed, thus only test `js` file
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   testEnvironment: 'node',
 };
