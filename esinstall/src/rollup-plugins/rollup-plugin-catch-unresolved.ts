99a34f7383029016a7cfc41e284c9db0ba205ac0
Drew Powers <1369770+drwpow@users.noreply.github.com>
2021-01-07 18:42:22 -0700

    Update Node builtins list (#2192)

--- esinstall/src/rollup-plugins/rollup-plugin-catch-unresolved.ts
+++ esinstall/src/rollup-plugins/rollup-plugin-catch-unresolved.ts
@@ -1,4 +1,4 @@
-import isNodeBuiltin from 'is-builtin-module';
+import builtinModules from 'builtin-modules';
 import {Plugin} from 'rollup';
 
 /**
@@ -14,7 +14,7 @@ export function rollupPluginCatchUnresolved(): Plugin {
       if (id.startsWith('http://') || id.startsWith('https://')) {
         return false;
       }
-      if (isNodeBuiltin(id)) {
+      if (builtinModules.indexOf(id) !== -1) {
         this.warn({
           id: importer,
           message: `Module "${id}" (Node.js built-in) is not available in the browser. Run Snowpack with --polyfill-node to fix.`,
