a871c85ab9ab2fc6d0ad9d6d20f20ff4aa192e90
Fred K. Schott <fkschott@gmail.com>
2021-01-09 11:10:57 -0800

    [WIP] Add SSR esm-runtime package (#2064)
    * add ESM runtime
    
    * checkpoint
    
    * move back into snowpack for now
    
    * update deps

--- snowpack/src/commands/dev.ts
+++ snowpack/src/commands/dev.ts
@@ -26,6 +26,7 @@
 
 import cacache from 'cacache';
 import isCompressible from 'compressible';
+import {createLoader as createSSRLoader, SSRLoader} from '../ssr-loader';
 import etag from 'etag';
 import {EventEmitter} from 'events';
 import {createReadStream, promises as fs, statSync} from 'fs';
@@ -242,6 +243,24 @@ function handleResponseError(req, res, err: Error | NotFoundError) {
   });
   sendResponseError(req, res, 500);
   return;
+}
+
+function getSSRLoader(
+  sp: SnowpackDevServer,
+  options: {invalidateOnChange?: boolean} = {},
+): SSRLoader {
+  const runtime = createSSRLoader({
+    load: (url) => sp.loadUrl(url, {isSSR: true, allowStale: false, encoding: 'utf8'}),
+  });
+  if (options.invalidateOnChange !== false) {
+    sp.onFileChange(({filePath}) => {
+      const url = sp.getUrlForFile(filePath);
+      if (url) {
+        runtime.invalidateModule(url);
+      }
+    });
+  }
+  return runtime;
 }
 
 export async function startDevServer(commandOptions: CommandOptions): Promise<SnowpackDevServer> {
@@ -1357,18 +1376,21 @@ export async function startDevServer(commandOptions: CommandOptions): Promise<Sn
   depWatcher.on('change', onDepWatchEvent);
   depWatcher.on('unlink', onDepWatchEvent);
 
-  return {
+  const sp = {
     port,
     loadUrl,
     handleRequest,
     sendResponseFile,
     sendResponseError,
+    getUrlForFile: (fileLoc: string) => getUrlForFile(fileLoc, config),
     onFileChange: (callback) => (onFileChangeCallback = callback),
+    getSSRLoader: (options) => getSSRLoader(sp, options),
     async shutdown() {
       await watcher.close();
       server.close();
     },
-  };
+  } as SnowpackDevServer;
+  return sp;
 }
 
 export async function command(commandOptions: CommandOptions) {
