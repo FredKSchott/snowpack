9f451a53e8c6e884c8d6a9eb31ea9a9242a3055c
Nate Moore <nate@skypack.dev>
2021-03-11 12:32:39 -0600

    chore: add postbuild script to sidestep rollup

--- snowpack/assets/require-or-import.js
+++ snowpack/assets/require-or-import.js
@@ -1,5 +1,8 @@
 "use strict";
 
+const NATIVE_REQUIRE = eval('require');
+const NATIVE_IMPORT = (filepath) => import(filepath);
+
 /**
  * A utility function to use Node's native `require` or dynamic `import` to load CJS or ESM files
  * @param {string} filepath 
@@ -7,11 +10,11 @@
 module.exports = async function requireOrImport(filepath) {
     return new Promise((resolve, reject) => {
         try {
-            let mdl = require(filepath);
+            let mdl = NATIVE_REQUIRE(filepath);
             resolve(mdl);
         } catch (e) {
             if (e.code === 'ERR_REQUIRE_ESM') {
-                return import(filepath).then(mdl => resolve(mdl.default ? mdl.default : mdl));
+                return NATIVE_IMPORT(filepath).then(mdl => resolve(mdl.default ? mdl.default : mdl));
             };
             reject(e);
         }
---
+++ snowpack/scripts/postbundle.js
@@ -0,0 +1,14 @@
+// HACK to bypass Rollup re-writing valid dynamic `import()` statement
+const fs = require('fs');
+const path = require('path');
+const rootDirectory = path.join(__dirname, '../..');
+
+function replaceNativeImport() {
+  const filepath = path.join(rootDirectory, 'snowpack', '/lib/index.js');
+
+  let contents = fs.readFileSync(filepath).toString();
+  contents = contents.replace(/^(const NATIVE_IMPORT =.*)$/gm, '').replace(/NATIVE_IMPORT\(/gm, 'import(');
+  fs.writeFileSync(filepath, contents);
+}
+
+replaceNativeImport();
