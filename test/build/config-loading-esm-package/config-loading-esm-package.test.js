874bb2b224b640964c448a48043af803c8dba17b
Nate Moore <nate@skypack.dev>
2021-03-11 11:00:12 -0600

    test: improve tests for supported config loading

---
+++ test/build/config-loading-esm-package/config-loading-esm-package.test.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const path = require('path');
+const {setupBuildTest} = require('../../test-utils');
+
+const cwd = path.join(__dirname, 'TEST_BUILD_OUT');
+
+describe('config-loading: detects snowpack.config.js inside of "type": "module" package', () => {
+  beforeAll(() => {
+    setupBuildTest(__dirname);
+  });
+
+  it('picks up on configured buildOptions.out', () => {
+    const distJSLoc = path.join(cwd, 'src', 'index.js');
+    expect(fs.existsSync(distJSLoc)).toBe(true); // JS file exists
+
+    const snowpackMetaLoc = path.join(cwd, '_snowpack', 'env.js');
+    expect(fs.existsSync(snowpackMetaLoc)).toBe(true); // snowpack meta exists
+  });
+});
--- test/build/config-loading/cjs/config-loading.test.js
+++
@@ -1,8 +0,0 @@
-const { loadConfiguration } = require('snowpack');
-
-describe('config loading', () => {
-  it('picks up snowpack.config.cjs', async () => {
-    const config = await loadConfiguration();
-    expect(config.buildOptions.out).toBe('TEST_BUILD_OUT');
-  });
-});
