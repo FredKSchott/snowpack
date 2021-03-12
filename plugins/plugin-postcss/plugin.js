68a66d4ebe1a92de5d5d2d430f035d9b10e8957a
Josh Wilson <joshwilsonvu@gmail.com>
2021-03-12 18:07:02 -0500

    plugin-postcss: Use API and threadpool instead of CLI. (#2165)
    Co-authored-by: Drew Powers <1369770+drwpow@users.noreply.github.com>

--- plugins/plugin-postcss/plugin.js
+++ plugins/plugin-postcss/plugin.js
@@ -1,21 +1,52 @@
-const execa = require('execa');
+'use strict';
+
+const {resolve} = require('path');
+const workerpool = require('workerpool');
 
 module.exports = function postcssPlugin(snowpackConfig, options) {
+  // options validation
+  if (options) {
+    if (typeof options !== 'object') throw new Error('options isn’t an object. Please see README.');
+    if (options.config && typeof options.config !== 'string')
+      throw new Error('options.config must be a path to a PostCSS config file.');
+  }
+
+  let worker, pool;
+
   return {
     name: '@snowpack/postcss-transform',
     async transform({fileExt, contents}) {
-      const {input = ['.css'], config} = options;
+      let {input = ['.css'], config} = options;
+
       if (!input.includes(fileExt) || !contents) return;
 
-      const flags = [];
-      if (config) flags.push(`--config ${config}`);
+      if (config) {
+        config = resolve(config);
+      }
 
-      const {stdout} = await execa('postcss', flags, {
+      pool = pool || workerpool.pool(require.resolve('./worker.js'));
+      worker = worker || (await pool.proxy());
+
+      const encodedResult = await worker.transformAsync(contents, {
+        config,
         cwd: snowpackConfig.root || process.cwd(),
-        input: contents,
+        map:
+          snowpackConfig.buildOptions && snowpackConfig.buildOptions.sourceMaps
+            ? {
+                prev: false,
+                annotation: false,
+                inline: false,
+              }
+            : false,
       });
+      const {code, map} = JSON.parse(encodedResult);
 
-      if (stdout) return stdout;
+      return {
+        '.css': {code, map},
+      };
+    },
+    cleanup() {
+      pool && pool.terminate();
     },
   };
 };
