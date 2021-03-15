62e0c45ed86a6d3c5952b0958748ea738fe59c64
natemoo-re <nate@natemoo.re>
2021-03-15 12:48:00 -0500

    chore: directly copy docs to www

---
+++ www/scripts/copy.js
@@ -0,0 +1,20 @@
+const fs = require('fs/promises');
+const path = require('path');
+
+const getAllFiles = async (dir) => {
+  const all = await fs.readdir(dir, { withFileTypes: true });
+  return [].concat(...await Promise.all(all.map(ent => ent.isFile() ? path.join(dir, ent.name) : getAllFiles(path.join(dir, ent.name)))));
+}
+
+async function copy() {
+  const docsDir = path.resolve(__dirname, '../../docs');
+  const docs = await getAllFiles(docsDir);
+
+  await Promise.all(docs.map(src => {
+    if (['README.md', '.DS_Store'].includes(path.basename(src))) return;
+    const dest = path.join(__dirname, '..', '_template', src.replace(docsDir, ''));
+    return fs.mkdir(path.dirname(dest), { recursive: true }).then(() => fs.copyFile(src, dest));
+  }));
+}
+
+copy()
