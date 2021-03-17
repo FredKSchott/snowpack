eb902befe849040724887ea2b725bd73aefc5c21
natemoo-re <nate@natemoo.re>
2021-03-17 10:05:37 -0500

    test: add jsxInject tests

---
+++ test/build/jsx-inject/jsx-inject.test.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const {setupBuildTest, readFiles} = require('../../test-utils');
+
+const cwd = path.join(__dirname, 'build');
+
+let files = {};
+
+describe('config: buildOptions.jsxInject', () => {
+  beforeAll(() => {
+    setupBuildTest(__dirname);
+    files = readFiles(cwd);
+  });
+
+  it('injects jsxInject string in JSX file', () => {
+    expect(files['/_dist_/jsx-file.js']).toContain('import {h, Fragment} from "../_snowpack/pkg/preact.js"');
+  });
+
+  it('injects jsxInject string in TSX file', () => {
+    expect(files['/_dist_/tsx-file.js']).toContain('import {h, Fragment} from "../_snowpack/pkg/preact.js"');
+  });
+
+  it('does not inject jsxInject string into JS file', () => {
+    expect(files['/_dist_/js-file.js']).not.toContain('import {h, Fragment} from "../_snowpack/pkg/preact.js"');
+  });
+
+  it('does not inject jsxInject string into TS file', () => {
+    expect(files['/_dist_/ts-file.js']).not.toContain('import {h, Fragment} from "../_snowpack/pkg/preact.js"');
+  });
+});
