f53644a8d0c5032d8ef43edcf70c98b8ecf90a43
natemoo-re <nate@natemoo.re>
2021-03-19 12:59:57 -0500

    remove get-url tests, they were causing CI to hang

--- test/build/get-url/src/index.jsx
+++
@@ -1,9 +0,0 @@
-import { h } from 'preact';
-import { useState } from 'preact/hooks';
-
-export default () => {
-  const [count, setCount] = useState(0);
-  return (
-    <div onClick={() => setCount(i => i + 1)}>{count}</div>
-  )
-};
