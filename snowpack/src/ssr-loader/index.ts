FredKSchott/snowpack

commit 689f58e96b2a8f1b53384d0ec34cd5388d7ccd0c

SSR fixes (#2853)
These SSR fixes all relate with the ability to run Vue SSR through snowpack. A few things this fixes:

* Export star support depends on the modules being enumerable, and real module namespace objects are, so this fixes that.
* Export statements get updated, so don't then try to update export identifiers that happen to match import identifiers.
* Use REQUIRE_OR_IMPORT to import a module, so that ESM modules can be imported.

snowpack/src/ssr-loader/index.ts
@@ -3,20 +3,26 @@ import {resolve} from 'url';
 import {ServerRuntime, ServerRuntimeConfig} from '../types';
 import {sourcemap_stacktrace} from './sourcemaps';
 import {transform} from './transform';
+import {REQUIRE_OR_IMPORT} from '../util';
 
 // This function makes it possible to load modules from the snowpack server, for the sake of SSR.
 export function createLoader({load}: ServerRuntimeConfig): ServerRuntime {
   const cache = new Map();
   const graph = new Map();
 
-  function getModule(importer: string, imported: string, urlStack: string[]) {
+  async function getModule(importer: string, imported: string, urlStack: string[]) {
     if (imported[0] === '/' || imported[0] === '.') {
       const pathname = resolve(importer, imported);
       if (!graph.has(pathname)) graph.set(pathname, new Set());
       graph.get(pathname).add(importer);
       return _load(pathname, urlStack);
     }
-    return Promise.resolve(nodeRequire(imported));
+    const mod = await REQUIRE_OR_IMPORT(imported);
+
+    return {
+      exports: mod,
+      css: []
+    };
   }
 
   function invalidateModule(path) {
@@ -71,7 +77,7 @@ export function createLoader({load}: ServerRuntimeConfig): ServerRuntime {
       {
         name: names.__export,
         value: (name, get) => {
-          Object.defineProperty(exports, name, {get});
+          Object.defineProperty(exports, name, {get, enumerable: true});
         },
       },
       {
@@ -80,6 +86,7 @@ export function createLoader({load}: ServerRuntimeConfig): ServerRuntime {
           for (const name in mod) {
             Object.defineProperty(exports, name, {
               get: () => mod[name],
+              enumerable: true
             });
           }
         },
@@ -141,20 +148,5 @@ export function createLoader({load}: ServerRuntimeConfig): ServerRuntime {
     invalidateModule: (url) => {
       invalidateModule(url);
     },
-  };
-}
-
-function nodeRequire(source) {
-  // mirror Rollup's interop by allowing both of these:
-  //  import fs from 'fs';
-  //  import { readFileSync } from 'fs';
-  return {
-    exports: new Proxy(require(source), {
-      get(mod, prop) {
-        if (prop === 'default') return mod;
-        return mod[prop];
-      },
-    }),
-    css: [],
   };
 }

snowpack/src/ssr-loader/transform.ts
@@ -157,6 +157,7 @@ export function transform(data) {
 
         if (!is_reference(node, parent)) return;
         if (!replacements.has(node.name)) return;
+        if(parent.type === 'ExportSpecifier') return;
 
         if (current_scope.find_owner(node.name) === scope) {
           let replacement = replacements.get(node.name);

commit 03f1d92979028b60b860631e78195986566e61f2

Remove enumerability of converted ESM in SSR
This fixes #2918

The behavior of Rollup's CommonJS plugin to allow CommonJS modules to treat the default export as a function relied on named exports being non-enumerable. This was regressed in 689f58e to fix another bug related to re-exporting everything from another module.

That code was using enumerability to re-export. We can fix both cases by not using enumerability in the export star code, but rather grabbing the property descriptors.

snowpack/src/ssr-loader/index.ts
@@ -77,18 +77,15 @@ export function createLoader({load}: ServerRuntimeConfig): ServerRuntime {
       {
         name: names.__export,
         value: (name, get) => {
-          Object.defineProperty(exports, name, {get, enumerable: true});
+          Object.defineProperty(exports, name, {get});
         },
       },
       {
         name: names.__export_all,
         value: (mod) => {
-          for (const name in mod) {
-            Object.defineProperty(exports, name, {
-              get: () => mod[name],
-              enumerable: true,
-            });
-          }
+          // Copy over all of the descriptors.
+          const descriptors = Object.getOwnPropertyDescriptors(mod);
+          Object.defineProperties(exports, descriptors);
         },
       },
       {