941110cdc6af20b8e004ccf3f37ff422ac8a0e1c
Fred K. Schott <fkschott@gmail.com>
2021-03-19 09:17:18 -0700

    fix esbuild 0.9.x breaking change (#2914)

---
+++ test/build/config-optimize-bundle/src/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
+import './index.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
+
+// Hot Module Replacement (HMR) - Remove this snippet to remove HMR.
+// Learn more: https://www.snowpack.dev/concepts/hot-module-replacement
+if (import.meta.hot) {
+  import.meta.hot.accept();
+}
