fcf7a7ffb01ec6ce6098a0def856a2dcac0c47db
YogliB <yogiboaron@gmail.com>
2021-03-20 18:45:36 +0200

    refactor(angular-plugin): remove unnecessary `async` statement

--- plugins/plugin-angular/plugin.js
+++ plugins/plugin-angular/plugin.js
@@ -1,75 +1,66 @@
-const { command, commandSync } = require('execa');
-const { env } = require('npm-run-path');
+const {command, commandSync} = require('execa');
+const {env} = require('npm-run-path');
 const cwd = process.cwd();
 
 function dataListener(chunk, log) {
-	let stdOutput = chunk.toString();
-	// In --watch mode, handle the "clear" character
-	if (stdOutput.includes('\u001bc') || stdOutput.includes('\x1Bc')) {
-		log('WORKER_RESET', {});
-		stdOutput = stdOutput.replace(/\x1Bc/, '').replace(/\u001bc/, '');
-	}
-	log('WORKER_MSG', { level: 'log', msg: `${stdOutput}` });
+  let stdOutput = chunk.toString();
+  // In --watch mode, handle the "clear" character
+  if (stdOutput.includes('\u001bc') || stdOutput.includes('\x1Bc')) {
+    log('WORKER_RESET', {});
+    stdOutput = stdOutput.replace(/\x1Bc/, '').replace(/\u001bc/, '');
+  }
+  log('WORKER_MSG', {level: 'log', msg: `${stdOutput}`});
 }
 
 function runNgc(args, log) {
-	const { stdout, stderr } = commandSync(
-		`ngc ${args || '--project ./tsconfig.app.json'}`,
-		{
-			env: env(),
-			extendEnv: true,
-			windowsHide: false,
-			cwd,
-		}
-	);
+  const {stdout, stderr} = commandSync(`ngc ${args || '--project ./tsconfig.app.json'}`, {
+    env: env(),
+    extendEnv: true,
+    windowsHide: false,
+    cwd,
+  });
 
-	if (stdout && stdout.trim()) dataListener(stdout, log);
-	if (stderr && stderr.trim()) dataListener(stderr, log);
+  if (stdout && stdout.trim()) dataListener(stdout, log);
+  if (stderr && stderr.trim()) dataListener(stderr, log);
 }
 
-function angularPlugin(_, { args } = {}) {
-	/**
-	 * @type {import('snowpack').SnowpackPlugin}
-	 */
-	const plugin = {
-		name: '@snowpack/plugin-angular',
-		async run({ isDev, log }) {
-			if (isDev) runNgc(args, log);
+function angularPlugin(_, {args} = {}) {
+  /**
+   * @type {import('snowpack').SnowpackPlugin}
+   */
+  const plugin = {
+    name: '@snowpack/plugin-angular',
+    run({isDev, log}) {
+      if (isDev) runNgc(args, log);
 
-			const workerPromise = command(
-				`ngc ${args || '--project ./tsconfig.app.json'} ${
-					isDev ? '--watch' : ''
-				}`,
-				{
-					env: env(),
-					extendEnv: true,
-					windowsHide: false,
-					cwd,
-				}
-			);
-			const { stdout, stderr } = workerPromise;
+      const workerPromise = command(
+        `ngc ${args || '--project ./tsconfig.app.json'} ${isDev ? '--watch' : ''}`,
+        {
+          env: env(),
+          extendEnv: true,
+          windowsHide: false,
+          cwd,
+        },
+      );
+      const {stdout, stderr} = workerPromise;
 
-			stdout && stdout.on('data', (chunk) => dataListener(chunk, log));
-			stderr && stderr.on('data', (chunk) => dataListener(chunk, log));
+      stdout && stdout.on('data', (chunk) => dataListener(chunk, log));
+      stderr && stderr.on('data', (chunk) => dataListener(chunk, log));
 
-			return workerPromise;
-		},
-		transform({ contents, fileExt, isDev }) {
-			if (isDev || fileExt.trim() !== '.js' || !contents.trim())
-				return contents;
+      return workerPromise;
+    },
+    transform({contents, fileExt, isDev}) {
+      if (isDev || fileExt.trim() !== '.js' || !contents.trim()) return contents;
 
-			const {
-				buildOptimizer,
-			} = require('@angular-devkit/build-optimizer');
+      const {buildOptimizer} = require('@angular-devkit/build-optimizer');
 
-			const transpiledContent = buildOptimizer({ content: contents })
-				.content;
+      const transpiledContent = buildOptimizer({content: contents}).content;
 
-			return transpiledContent || contents;
-		},
-	};
+      return transpiledContent || contents;
+    },
+  };
 
-	return plugin;
+  return plugin;
 }
 
 module.exports = angularPlugin;
