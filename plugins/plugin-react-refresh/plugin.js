b07d815ba6335995e8773e7267e171f9aa056f73
FredKSchott <FredKSchott@users.noreply.github.com>
2021-03-30 19:27:07 +0000

    [ci] yarn format

--- plugins/plugin-react-refresh/plugin.js
+++ plugins/plugin-react-refresh/plugin.js
@@ -13,10 +13,8 @@ const reactRefreshCode = fs
   .replace(`process.env.NODE_ENV`, JSON.stringify('development'));
 
 function transformHtml(contents) {
-  return contents.replace(
-    /(.*)(<body.*?>)(.*)/s,
-    function (match, p1, p2, p3) {  
-      return `${p1}${p2}
+  return contents.replace(/(.*)(<body.*?>)(.*)/s, function (match, p1, p2, p3) {
+    return `${p1}${p2}
 <script>
   function debounce(e,t){let u;return()=>{clearTimeout(u),u=setTimeout(e,t)}}
   {
@@ -28,9 +26,8 @@ function transformHtml(contents) {
     window.$RefreshReg$ = () => {};
     window.$RefreshSig$ = () => (type) => type;
   }
-</script>${p3}`
-    },
-  );
+</script>${p3}`;
+  });
 }
 
 const babel = require('@babel/core');
