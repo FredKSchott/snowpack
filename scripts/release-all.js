6049160088995a39b137afc4ea4299e47b6a7fb5
FredKSchott <FredKSchott@users.noreply.github.com>
2021-03-31 19:40:11 +0000

    [ci] yarn format

--- scripts/release-all.js
+++ scripts/release-all.js
@@ -1,4 +1,3 @@
-
 // How to release many packages at once:
 // 1. get a list of what everything is meant to be going out (git diff [LAST RELEASE COMMIT] --stat)
 // 2. create the release-all.js script (get the patch vs. minor vs. major for each package going out. ex: git diff [LAST RELEASE COMMIT] plugins/plugin-svelte)
@@ -7,15 +6,15 @@
 
 const release = require('./release.cjs');
 
-release('esinstall', 'latest', 'patch'. true);
-release('skypack', 'latest', 'patch'. true);
-release('snowpack', 'latest', 'patch'. true);
-release('app-template-blank-typescript', 'latest', 'patch'. true);
-release('app-template-lit-element', 'latest', 'patch'. true);
-release('app-template-preact-typescript', 'latest', 'patch'. true);
-release('app-template-react-typescript', 'latest', 'patch'. true);
-release('app-template-svelte-typescript', 'latest', 'patch'. true);
-release('app-template-svelte-typescript', 'latest', 'patch'. true);
-release('app-template-vue-typescript', 'latest', 'patch'. true);
-release('plugins/plugin-optimize', 'latest', 'patch'. true);
-release('plugins/web-test-runner-plugin', 'latest', 'patch'. true);
+release('esinstall', 'latest', 'patch'.true);
+release('skypack', 'latest', 'patch'.true);
+release('snowpack', 'latest', 'patch'.true);
+release('app-template-blank-typescript', 'latest', 'patch'.true);
+release('app-template-lit-element', 'latest', 'patch'.true);
+release('app-template-preact-typescript', 'latest', 'patch'.true);
+release('app-template-react-typescript', 'latest', 'patch'.true);
+release('app-template-svelte-typescript', 'latest', 'patch'.true);
+release('app-template-svelte-typescript', 'latest', 'patch'.true);
+release('app-template-vue-typescript', 'latest', 'patch'.true);
+release('plugins/plugin-optimize', 'latest', 'patch'.true);
+release('plugins/web-test-runner-plugin', 'latest', 'patch'.true);
