01f94f9d399a6bb60493f31df3f5e1ab675dbdb0
lukejacksonn <lukejacksonn@gmail.com>
2021-04-06 10:18:41 +0100

    Migrates extends plugins test [WIP]

---
+++ test/snowpack/config/extends-plugins/index.test.js
@@ -0,0 +1,45 @@
+const {testFixture} = require('../../../fixture-utils');
+const dedent = require('dedent');
+
+describe('extends-plugins', () => {
+  beforeAll(() => {
+    // Needed until we make Snowpack's JS Build Interface quiet by default
+    require('snowpack').logger.level = 'warn';
+  });
+
+  it('Loads and uses the appropriate plugins', async () => {
+    const result = await testFixture(
+      {
+        extends: './base/snowpack.config.json',
+        plugins: ['@snowpack/plugin-sass'],
+      },
+      {
+        'base/package.json': dedent`
+          {
+            "private": true,
+            "version": "1.0.0",
+            "name": "@snowpack/test-config-extends-plugins-base",
+            "devDependencies": {
+              "@snowpack/plugin-dotenv": "^1.6.0-alpha.0"
+            }
+          } 
+        `,
+        'base/snowpack.config.json': dedent`
+          {
+            "plugins": ["@snowpack/plugin-dotenv"]
+          }        
+        `,
+        'index.js': dedent`
+          console.log(import.meta.env.SNOWPACK_PUBLIC_SECRET_VALUE);
+        `,
+        '.env': dedent`
+          SNOWPACK_PUBLIC_SECRET_VALUE=pumpernickel
+        `,
+      },
+    );
+
+    expect(result['_snowpack/env.js']).toEqual(
+      expect.stringContaining(`export const SNOWPACK_PUBLIC_SECRET_VALUE = "pumpernickel";`),
+    );
+  });
+});
