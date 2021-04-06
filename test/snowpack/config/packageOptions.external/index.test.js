e9a87aa485cd2e425d75e7fddb5fe06ff512a818
lukejacksonn <lukejacksonn@gmail.com>
2021-04-06 20:33:11 +0100

    Migrates config packageOptions.external test

---
+++ test/snowpack/config/packageOptions.external/index.test.js
@@ -0,0 +1,26 @@
+const {testFixture} = require('../../../fixture-utils');
+const dedent = require('dedent');
+
+describe('packageOptions.external', () => {
+  beforeAll(() => {
+    // Needed until we make Snowpack's JS Build Interface quiet by default
+    require('snowpack').logger.level = 'warn';
+  });
+
+  it('Does not transform imports of external modules', async () => {
+    const result = await testFixture(
+      {
+        packageOptions: {
+          external: ['fs'],
+        },
+      },
+      {
+        'index.js': dedent`
+          import 'fs';
+          import 'array-flatten';
+        `,
+      },
+    );
+    expect(result['index.js']).toEqual(expect.stringContaining(`import 'fs';`));
+  });
+});
