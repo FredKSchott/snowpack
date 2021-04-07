94bea848e34b6cf2d01e97f1bbd9dff5a016f4e0
lukejacksonn <lukejacksonn@gmail.com>
2021-04-08 00:08:22 +0100

    Migrates html env variables test [WIP]

--- test/snowpack/config/env/index.test.js
+++ test/snowpack/config/env/index.test.js
@@ -22,4 +22,51 @@ describe('env', () => {
     );
     expect(result['_snowpack/env.js']).toContain('export const API_URL = "TEST";');
   });
+
+  // Reports back that SNOWPACK_PUBLIC_MY_ENV_VAR is not set
+  it.skip('Should inject env variables into HTML', async () => {
+    const result = await testFixture(
+      {
+        env: {
+          SNOWPACK_PUBLIC_MY_ENV_VAR: 'my-var-replacement',
+          SNOWPACK_PUBLIC_: 'ignoreme',
+        },
+      },
+      {
+        'index.html': dedent`
+          <!DOCTYPE html>
+          <html
+            lang="en"
+            data-mode="%MODE%"
+            data-public-url="%PUBLIC_URL%"
+            data-my-env-var="%SNOWPACK_PUBLIC_MY_ENV_VAR%"
+            data-edge-case-test="%SNOWPACK_PUBLIC_%"
+            data-undefined="%SNOWPACK_PUBLIC_BUILD_UNDEFINED%"
+          >
+            <head>
+              <meta charset="utf-8" />
+              <link rel="icon" href="/favicon.ico" />
+              <meta name="viewport" content="width=device-width, initial-scale=1" />
+              <meta name="description" content="Web site created using create-snowpack-app" />
+              <link rel="stylesheet" type="text/css" href="/index.css" />
+              <title>Snowpack App</title>
+            </head>
+            <body></body>
+          </html>
+        `,
+      },
+    );
+
+    expect(result['index.html']).toEqual(expect.stringContaining('lang="en"'));
+    expect(result['index.html']).toEqual(expect.stringContaining('data-mode="production"'));
+    expect(result['index.html']).toEqual(
+      expect.stringContaining('data-my-env-var="my-var-replacement"'),
+    );
+    expect(result['index.html']).toEqual(
+      expect.stringContaining('data-edge-case-test="%SNOWPACK_PUBLIC_%"'),
+    );
+    expect(result['index.html']).toEqual(
+      expect.stringContaining('data-undefined="%SNOWPACK_PUBLIC_BUILD_UNDEFINED%"'),
+    );
+  });
 });
