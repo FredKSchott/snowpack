aef86b587a01af7e44cba747e79674a17ef69a0b
lukejacksonn <lukejacksonn@gmail.com>
2021-04-12 20:26:43 +0100

    Migrates utf8 test

---
+++ test/snowpack/utf8/index.test.js
@@ -0,0 +1,46 @@
+const {testFixture} = require('../../fixture-utils');
+const dedent = require('dedent');
+
+describe('utf8', () => {
+  beforeAll(() => {
+    // Needed until we make Snowpack's JS Build Interface quiet by default
+    require('snowpack').logger.level = 'error';
+  });
+
+  it('Unicode characters are not escaped', async () => {
+    const result = await testFixture(
+      {
+        optimize: {
+          bundle: true,
+          minify: true,
+        },
+      },
+      {
+        'index.js': dedent`
+          export function getText() {
+            return 'testing utf-8 characters: юникод не эскейпится 👌';
+          }
+          
+          document.body.append(getText());
+        `,
+        'index.html': dedent`
+          <!DOCTYPE html>
+          <html lang="ru">
+            <head>
+              <meta charset="utf-8" />
+              <title>utf-8 test</title>
+              <script src="index.js" type="module" defer></script>
+            </head>
+            <body>
+              <h1>Testing UTF-8: проверка юникода</h1>
+            </body>
+          </html> 
+        `,
+      },
+    );
+    // Unicode characters aren't escaped in html
+    expect(result['index.html']).toContain('проверка юникода');
+    // Unicode characters aren't escaped in typescript
+    expect(result['index.js']).toContain('юникод не эскейпится 👌');
+  });
+});
