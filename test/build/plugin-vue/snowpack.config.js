8023627f8196a04b0c7659df1133debfeb6b86e5
lukejacksonn <lukejacksonn@gmail.com>
2021-04-12 19:42:45 +0100

    Migrates plugin vue test

--- test/build/plugin-vue/snowpack.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  mount: {
-    src: '/_dist_',
-  },
-  plugins: ['@snowpack/plugin-vue'],
-};
---
+++ test/snowpack/plugin/vue/index.test.js
@@ -0,0 +1,46 @@
+const {testFixture} = require('../../../fixture-utils');
+const dedent = require('dedent');
+
+describe('plugin', () => {
+  beforeAll(() => {
+    // Needed until we make Snowpack's JS Build Interface quiet by default
+    require('snowpack').logger.level = 'error';
+  });
+
+  it('@snowpack/plugin-vue', async () => {
+    const result = await testFixture(
+      {
+        plugins: [['@snowpack/plugin-vue']],
+      },
+      {
+        'MyComponent.vue': dedent`
+          <script>
+            export default {
+              props: {}
+            }
+          </script>
+        `,
+        'index.vue': dedent`
+          <script>
+            import MyComponent from './MyComponent';
+            export default {
+              components: { MyComponent }
+            }
+          </script>
+        `,
+        'package.json': dedent`
+          {
+            "version": "1.0.0",
+            "name": "@snowpack/test-plugin-vue",
+            "devDependencies": {
+              "@snowpack/plugin-vue": "^2.3.0"
+            }
+          }  
+        `,
+      },
+    );
+
+    expect(result['MyComponent.vue.js']).toBeDefined();
+    expect(result['index.vue.js']).toContain(`import MyComponent from './MyComponent.vue.js'`);
+  });
+});
