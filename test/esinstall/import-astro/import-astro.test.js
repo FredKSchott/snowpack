afc6232f91d3d6c2988135a56379b08ee1f9c71e
Matthew Phillips <matthew@matthewphillips.info>
2021-06-03 16:39:04 -0400

    Allow .astro files to be installed as JavaScript (#3406)
    This is a stopgap solution. In the future we will have a better to control what file types get build as JavaScript by esinstall.

---
+++ test/esinstall/import-astro/import-astro.test.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const {runTest} = require('../esinstall-test-utils.js');
+
+describe('importing .astro files', () => {
+  it('included in the bundle', async () => {
+    const cwd = __dirname;
+    const dest = path.join(cwd, 'test-astro');
+
+    // Run Test
+    const { importMap: { imports }} = await runTest(['astro-components/Wow.astro'], {
+      cwd, dest,
+      rollup: {
+        plugins: [
+          {
+            load() {
+              return 'export default "so wow";'
+            }
+          }
+        ]
+      }
+    });
+    
+    expect(imports['astro-components/Wow.astro']).toBe('./astro-components/Wow.astro.js');
+  });
+});
