64b377e3944d7ce310ab3bebb1ec50e841f2556a
Matthew Phillips <matthew@matthewphillips.info>
2021-06-03 16:53:38 -0400

    Correctly handle subpackage imports marked as external (#3407)
    * Correctly handle subpackage imports marked as external
    
    * Remove the .only

--- test/snowpack/config/packageOptions.external/index.test.js
+++ test/snowpack/config/packageOptions.external/index.test.js
@@ -24,4 +24,44 @@ describe('packageOptions.external', () => {
 
     expect(result['index.js']).toContain(`import 'fs';`);
   });
+
+  it('Does not install externals that are deep package imports', async () => {
+    const result = await testFixture({
+      'packages/some-thing/main.js': dedent`
+        export default 'ok';
+      `,
+      'packages/some-thing/deep.js': dedent`
+        export default 'oops';
+      `,
+      'packages/some-thing/package.json': dedent`
+        {
+          "version": "1.0.0",
+          "name": "some-thing",
+          "module": "main.js"
+        }
+      `,
+      'package.json': dedent`
+        {
+          "version": "1.0.1",
+          "name": "@snowpack/test-config-external",
+          "dependencies": {
+            "some-thing": "file:./packages/some-thing"
+          }
+        }
+      `,
+      'index.js': dedent`
+        import 'some-thing';
+        import 'some-thing/deep.js';
+      `,
+      'snowpack.config.js': dedent`
+        module.exports = {
+          packageOptions: {
+            external: ['some-thing/deep.js']
+          }
+        }
+      `
+    });
+
+    expect(result['index.js']).toContain(`import 'some-thing/deep.js';`);
+  })
 });
