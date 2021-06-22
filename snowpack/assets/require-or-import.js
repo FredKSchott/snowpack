52a44749cd9e00945ecd7354f988d094bcc0e7f4
Matthew Phillips <matthew@skypack.dev>
2021-06-22 11:25:22 -0400

    Allow cjs to be imported and use the default in SSR

--- snowpack/assets/require-or-import.js
+++ snowpack/assets/require-or-import.js
@@ -20,16 +20,28 @@ module.exports = async function requireOrImport(filepath, { from = process.cwd()
         });
         try {
           const mdl = NATIVE_REQUIRE(resolvedPath);
+
+          // Add a `default` property on a CommonJS export so that it can be accessed from import statements.
+          // This is set to enumerable: false to make it hidden(ish) to code.
+          if(mdl && !('default' in mdl)) {
+            Object.defineProperty(mdl, 'default', {
+              configurable: true,
+              enumerable: false,
+              writable: true,
+              value: mdl
+            });
+          }
+
           resolve(mdl);
         } catch (e) {
           if (e instanceof SyntaxError && /export|import/.test(e.message)) {
             console.error(`Failed to load "${filepath}"!\nESM format is not natively supported in "node@${process.version}".\nPlease use CommonJS or upgrade to an LTS version of node above "node@12.17.0".`)
           } else if (e.code === 'ERR_REQUIRE_ESM') {
               const url = pathToFileURL(resolvedPath);
-              return NATIVE_IMPORT(url).then(mdl => resolve(mdl.default ? mdl.default : mdl));
-          };
+              return NATIVE_IMPORT(url).then(mdl => resolve(mdl), err => reject(err));
+          }
           try {
-            return NATIVE_IMPORT(pathToFileURL(resolvedPath)).then(mdl => resolve(mdl.default ? mdl.default : mdl));
+            return NATIVE_IMPORT(pathToFileURL(resolvedPath)).then(mdl => resolve(mdl), err => reject(err));
           } catch (e) {
             reject(e);  
           }
