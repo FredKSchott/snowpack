eeedba7ccbbd3fabcfa269a596ef094db8ecfb49
Drew Powers <drew@pow.rs>
2021-06-25 16:47:47 -0600

    Fix Snowpack version on website

--- www/astro.config.mjs
+++ www/astro.config.mjs
@@ -3,11 +3,14 @@ export default {
   pages: './src/pages',
   dist: './dist',
   public: './public',
+  buildOptions: {
+    site: 'https://snowpack.dev/',
+  },
   renderers: [
     // When testing, we used this as a bit of a kitchen sink for Astro.
     // Keep the different mixing of frameworks just for fun, to show Astro off.
     '@astrojs/renderer-vue',
     '@astrojs/renderer-svelte',
-    '@astrojs/renderer-preact'
-  ]
+    '@astrojs/renderer-preact',
+  ],
 };
