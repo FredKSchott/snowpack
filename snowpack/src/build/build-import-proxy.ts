fab56b4bac311cae90d5334173d83f2a416138f6
Sander Maijers <3374183+sanmai-NL@users.noreply.github.com>
2021-07-07 09:04:18 +0200

    Remove superfluous script language attribute

--- snowpack/src/build/build-import-proxy.ts
+++ snowpack/src/build/build-import-proxy.ts
@@ -115,7 +115,7 @@ Add the missing doctype, or set buildOptions.htmlFragments=true if HTML fragment
   if (hmr && isFullPage) {
     let hmrScript = ``;
     if (hmrPort) {
-      hmrScript += `<script type="text/javascript">window.HMR_WEBSOCKET_PORT=${hmrPort}</script>\n`;
+      hmrScript += `<script>window.HMR_WEBSOCKET_PORT=${hmrPort}</script>\n`;
     }
     hmrScript += `<script type="module" integrity="${SRI_CLIENT_HMR_SNOWPACK}" src="${getMetaUrlPath(
       'hmr-client.js',
