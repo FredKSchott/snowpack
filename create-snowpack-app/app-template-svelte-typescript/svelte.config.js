fed2c940dfd692e50eedf1f541a2d54c55d6994d
Simon H <5968653+dummdidumm@users.noreply.github.com>
2021-08-24 22:41:07 +0200

    Remove default language option from app-template-svelte-typescript (#3662)
    * Remove default language option from app-template-svelte-typescript
    
    It's discouraged to use: https://github.com/sveltejs/svelte-preprocess/blob/main/docs/preprocessing.md#auto-preprocessing-options
    
    * changeset
    
    Co-authored-by: Simon Holthausen <simon.holthausen@accso.de>

--- create-snowpack-app/app-template-svelte-typescript/svelte.config.js
+++ create-snowpack-app/app-template-svelte-typescript/svelte.config.js
@@ -1,9 +1,5 @@
 const autoPreprocess = require('svelte-preprocess');
 
 module.exports = {
-  preprocess: autoPreprocess({
-    defaults: {
-      script: 'typescript',
-    },
-  }),
+  preprocess: autoPreprocess(),
 };
